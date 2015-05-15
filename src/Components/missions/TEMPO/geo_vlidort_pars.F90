cc56e17cf87262f3ba0dffa38eb9587d65a44b65
Patricia Castellanos <patricia.castellanos@nasa.gov>
2015-05-15 10:04:14 -0400

    initial commit. moved some parameter definitions from main geo_vlidort code to clean things up

---
+++ src/Components/missions/TEMPO/geo_vlidort_pars.F90
@@ -0,0 +1,39 @@
+
+!	----------------------
+!	This is an include file to define some constants used in the geo_vlidort 
+!	program and subroutines
+!-------------------------
+
+	! VLIDORT array sizes
+	integer, parameter                    :: nobs = 1                   ! number of profiles VLIDORT will work on
+  real, parameter                       :: ptop = 1.0             ! top (edge) pressure [Pa]
+  integer, parameter                    :: nch = 1                    ! number of channels
+  integer, parameter                    :: nq = 14                ! number of tracers
+  real, dimension(nch),parameter        :: channels = (/550.0/)    ! channels to simulate
+
+  ! Verbose
+  integer, parameter                    :: verbose = 1
+  integer, parameter                    :: verbose_mp = 0
+
+  ! MODIS kernel stuff
+  integer, parameter                    :: nkernel = 3
+  integer, parameter                    :: nparam  = 2
+  integer, parameter    :: nbands = 7                ! number of modis bands
+  integer, dimension(nch), parameter    :: bands = (/4/)             ! modis band that overlaps with vlidort channel
+
+  ! Missing Value 
+  real, parameter                       :: MISSING = -999
+
+  ! RC file
+  character(len=*), parameter           :: rcfile = 'Aod_EOS.rc'  ! resource file
+  character(len=16), parameter          :: vnames_string(nq) = (/'du001', 'du002', 'du003', 'du004', 'du005', &
+                                                             'ss001', 'ss002', 'ss003', 'ss004', 'ss005', &
+                                                             'BCphobic', 'BCphilic',                      &
+                                                             'OCphobic', 'OCphilic'/) ! array of variable name strings
+  character                             :: vnames(nq,16)          ! character array of variable names
+
+  ! Physical constants
+  real, parameter       								:: grav = 9.81               ! gravity
+
+  ! Cloud Filtering
+  real, parameter                     	:: cldmax = 0.01  
