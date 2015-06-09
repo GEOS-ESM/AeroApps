60e440ca450b299e8f029e9cd02998e8f5d5024a
Patricia Castellanos <patricia.castellanos@nasa.gov>
2015-06-09 14:33:07 -0400

    modified:   geo_vlidort_pars.F90 	PC: the modis band index is no longer a parameter.  should be figured in main geo_vlidort

--- src/Components/missions/TEMPO/geo_vlidort_pars.F90
+++ src/Components/missions/TEMPO/geo_vlidort_pars.F90
@@ -7,27 +7,26 @@
 	! VLIDORT array sizes
 	integer, parameter                    :: nobs = 1               ! number of profiles VLIDORT will work on
   real, parameter                       :: ptop = 1.0             ! top (edge) pressure [Pa]
-  integer, parameter                    :: nq = 14                ! number of tracers
+  integer, parameter                    :: nq   = 14                ! number of tracers
 
   ! Vector calculation constants
   integer, parameter 										:: nPol = 6                ! number of components of the scattering matrix
   integer, parameter 										:: nMom = 300 						 ! number of phase function moments - 300 is max for dust
 
   ! Verbose
-  integer, parameter                    :: verbose = 0
+  integer, parameter                    :: verbose    = 0
   integer, parameter                    :: verbose_mp = 0
 
   ! MODIS kernel stuff
   integer, parameter                    :: nkernel = 3
   integer, parameter                    :: nparam  = 2
-  integer, parameter    :: nbands = 7                ! number of modis bands
-  integer, dimension(nch), parameter    :: bands = (/4/)             ! modis band that overlaps with vlidort channel
+  integer, parameter                    :: nbands  = 7                ! number of modis bands
 
   ! Missing Value 
   real, parameter                       :: MISSING = 1e15
 
   ! RC file
-  character(len=*), parameter           :: rcfile = 'Aod_EOS.rc'  ! resource file
+  character(len=*), parameter           :: rcfile            = 'Aod_EOS.rc'  ! resource file
   character(len=16), parameter          :: vnames_string(nq) = (/'du001', 'du002', 'du003', 'du004', 'du005', &
                                                              'ss001', 'ss002', 'ss003', 'ss004', 'ss005', &
                                                              'BCphobic', 'BCphilic',                      &
