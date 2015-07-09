2df9b2e5c1a48a825a7ca821095f7f5e0b8079a0
Patricia Castellanos <patricia.castellanos@nasa.gov>
2015-07-09 16:34:32 -0400

    modified:   LIDORT_BRDF_MODIS.F90 	PC: made compatible with LIDORT functions

--- src/Components/missions/TEMPO/LIDORT_BRDF_MODIS.F90
+++ src/Components/missions/TEMPO/LIDORT_BRDF_MODIS.F90
@@ -1,13 +1,13 @@
-module VLIDORT_BRDF_MODIS   
+module LIDORT_BRDF_MODIS   
 !
-!  Simple f77 wrapper for the Python interface to VLIDORT for OMI aerosol
+!  Simple f77 wrapper for the Python interface to LIDORT for OMI aerosol
 !  channels.
 !
 !.............................................................................
   implicit NONE
 
-  PUBLIC Scalar_LandMODIS
-  PUBLIC Vector_LandMODIS
+  PUBLIC LIDORT_Scalar_LandMODIS
+  PUBLIC LIDORT_Vector_LandMODIS
 
   contains
 
@@ -19,15 +19,17 @@ module VLIDORT_BRDF_MODIS
     return
   end function IS_MISSING
 
-  subroutine VLIDORT_Scalar_LandMODIS (km, nch, nobs,channels,        &
+  subroutine LIDORT_Scalar_LandMODIS (km, nch, nobs,channels,        &
                      tau, ssa, g, pe, he, te, kernel_wt, param, &
                      solar_zenith, relat_azymuth, sensor_zenith, &
                      MISSING,verbose,radiance_VL_SURF,reflectance_VL_SURF, ROT, BR, rc, &
-                     albedo, reflectance_VL,radiance_VL )
+                     albedo, reflectance_L,radiance_L )
   !
-  ! Uses VLIDORT in scalar mode to compute OMI aerosol TOA radiances.
+  ! Uses LIDORT in scalar mode to compute OMI aerosol TOA radiances.
   !
-    use VLIDORT_ScatMod
+    use LIDORT_ScatMod
+
+    logical,parameter             :: aerosol = .true.   ! whether or not simulation contains aerosol
 
     implicit NONE
 
@@ -67,27 +69,25 @@ module VLIDORT_BRDF_MODIS
 
   ! !OUTPUT PARAMETERS:
 
-    real*8,           intent(out) :: radiance_VL_SURF(nobs,nch)       ! TOA normalized radiance from VLIDORT using surface module
-    real*8,           intent(out) :: reflectance_VL_SURF(nobs, nch)   ! TOA reflectance from VLIDORT using surface module
+    real*8,           intent(out) :: radiance_L_SURF(nobs,nch)       ! TOA normalized radiance from LIDORT using surface module
+    real*8,           intent(out) :: reflectance_L_SURF(nobs, nch)   ! TOA reflectance from LIDORT using surface module
     integer,          intent(out) :: rc                               ! return code
     real*8,           intent(out) :: ROT(km,nobs,nch)                 ! rayleigh optical thickness  
     real*8,           intent(out) :: BR(nobs,nch)                     ! bi-directional reflectance 
-    real*8, optional, intent(out) :: reflectance_VL(nobs, nch)        ! TOA reflectance from VLIDORT using albedo
-    real*8, optional, intent(out) :: radiance_VL(nobs,nch)            ! TOA normalized radiance from VLIDORT using albedo
+    real*8, optional, intent(out) :: reflectance_L(nobs, nch)        ! TOA reflectance from LIDORT using albedo
+    real*8, optional, intent(out) :: radiance_L(nobs,nch)            ! TOA normalized radiance from LIDORT using albedo
 
-    real*8                        :: Q(nobs, nch)                     ! Stokes parameter Q
-    real*8                        :: U(nobs, nch)                     ! Stokes parameter U  
   !  real*8,           intent(out) :: BRDF(nobs, nch)  
   !                         ---  
     integer             :: i,j,n,p,ier
 
   
-    type(VLIDORT_scat) :: SCAT
+    type(LIDORT_scat) :: SCAT
 
     rc = 0
     ier = 0
 
-    call VLIDORT_Init( SCAT%Surface%Base, km, rc)
+    call LIDORT_Init( SCAT%Surface%Base, km, rc)
     if ( rc /= 0 ) return
 
     do j = 1,nobs
@@ -98,8 +98,8 @@ module VLIDORT_BRDF_MODIS
            IS_MISSING(sensor_zenith(j),MISSING) .OR. &
            IS_MISSING(relat_azymuth(j),MISSING)  )  then
 
-        radiance_VL(j,:) = MISSING
-        reflectance_VL(j,:) = MISSING
+        radiance_L(j,:) = MISSING
+        reflectance_L(j,:) = MISSING
         cycle
       end if
         
@@ -113,15 +113,15 @@ module VLIDORT_BRDF_MODIS
         ! Make sure kernel weights and parameters are defined
         do n = 1, nkernel
           if (IS_MISSING(kernel_wt(n,i,j),MISSING)) then
-            radiance_VL_SURF(j,i) = MISSING
-            reflectance_VL_SURF(j,i) = MISSING
+            radiance_L_SURF(j,i) = MISSING
+            reflectance_L_SURF(j,i) = MISSING
             cycle
           end if
         end do
         do p = 1, nparam
           if (IS_MISSING(param(p,i,j),MISSING)) then
-            radiance_VL_SURF(j,i) = MISSING
-            reflectance_VL_SURF(j,i) = MISSING
+            radiance_L_SURF(j,i) = MISSING
+            reflectance_L_SURF(j,i) = MISSING
             cycle
           end if
         end do
@@ -129,11 +129,10 @@ module VLIDORT_BRDF_MODIS
         if ( verbose > 0 ) then
           print*, 'DO MODIS BRDF'
         end if
-        call VLIDORT_LANDMODIS(SCAT%Surface,solar_zenith(j),&
+        call LIDORT_LANDMODIS(SCAT%Surface,solar_zenith(j),&
                                sensor_zenith(j),relat_azymuth(j),&
                                kernel_wt(1,i,j),kernel_wt(2,i,j),kernel_wt(3,i,j),&
-                               reshape(param(:,i,j),(/nparam/)),&
-                               .true.,rc)
+                               reshape(param(:,i,j),(/nparam/)),rc)
 
         if ( rc /= 0 ) return
 
@@ -142,18 +141,18 @@ module VLIDORT_BRDF_MODIS
         SCAT%ssa => ssa(:,i,j)
         SCAT%g => g(:,i,j)
          
-        call VLIDORT_Run (SCAT, radiance_VL_SURF(j,i), reflectance_VL_SURF(j,i), &
-                          ROT(:,j,i), Q(j,i), U(j,i), .true., .true., ier)
+        call LIDORT_Run (SCAT, radiance_L_SURF(j,i), reflectance_L_SURF(j,i), &
+                          ROT(:,j,i), Q(j,i), U(j,i), aerosol, ier)
 
         BR(j,i) = SCAT%Surface%Base%VIO%VBRDF_Sup_Out%BS_DBOUNCE_BRDFUNC(1,1,1,1)
 
         if ( verbose > 0 ) then
-          print *, 'My radiance land modis',radiance_VL_SURF(j,i), reflectance_VL_SURF(j,i) 
+          print *, 'My radiance land modis',radiance_L_SURF(j,i), reflectance_L_SURF(j,i) 
         end if
 
         if ( ier /= 0 ) then
-          radiance_VL(j,i) = MISSING
-          reflectance_VL(j,i) = MISSING
+          radiance_L(j,i) = MISSING
+          reflectance_L(j,i) = MISSING
           cycle
         end if
 
@@ -168,8 +167,8 @@ module VLIDORT_BRDF_MODIS
             print *, 'albedo present'
         end if 
         if ( IS_MISSING(albedo(j,i),MISSING) ) then
-          radiance_VL(j,i) = MISSING
-          reflectance_VL(j,i) = MISSING
+          radiance_L(j,i) = MISSING
+          reflectance_L(j,i) = MISSING
           cycle
         end if
         else
@@ -181,221 +180,30 @@ module VLIDORT_BRDF_MODIS
         if ( verbose > 0 ) then
           print*, 'DO SURFACE LAMB'
         end if
-        call VLIDORT_SurfaceLamb(SCAT%Surface,albedo(j,i),solar_zenith (j),sensor_zenith(j),&
-                               relat_azymuth(j),.true.)
+        call LIDORT_SurfaceLamb(SCAT%Surface,albedo(j,i),solar_zenith (j),sensor_zenith(j),&
+                               relat_azymuth(j))
          
-        call VLIDORT_Run (SCAT, radiance_VL(j,i), reflectance_VL(j,i), &
-                        ROT(:,j,i), Q(j,i), U(j,i), .true., .true., ier)
+        call LIDORT_Run (SCAT, radiance_L(j,i), reflectance_L(j,i), &
+                        ROT(:,j,i), Q(j,i), U(j,i), aerosol, ier)
         if ( verbose > 0 ) then
-          print *, 'radiance albedo',albedo(j,i),radiance_VL(j,i), reflectance_VL(j,i) 
+          print *, 'radiance albedo',albedo(j,i),radiance_L(j,i), reflectance_L(j,i) 
         end if
         if ( ier /= 0 ) then
-          radiance_VL(j,i) = MISSING
-          reflectance_VL(j,i) = MISSING
+          radiance_L(j,i) = MISSING
+          reflectance_L(j,i) = MISSING
           cycle
         end if
       end do ! end loop over channels
      
       if ( verbose > 0 ) then
         if ( mod(j-1,1000) == 0 ) then
-          print *, '<> VLIDORT Scalar: ', nint(j*100./nobs), '%'
+          print *, '<> LIDORT Scalar: ', nint(j*100./nobs), '%'
         end if
       end if
 
     end do ! Loop over obs
 
-  end subroutine VLIDORT_Scalar_LandMODIS
+  end subroutine LIDORT_Scalar_LandMODIS
 
   !..........................................................................
-
-  subroutine VLIDORT_Vector_LandMODIS (km, nch, nobs, channels, nMom,  &
-                     nPol,tau, ssa, g, pmom, pe, he, te, kernel_wt, param, &
-                     solar_zenith, relat_azymuth, sensor_zenith, &
-                     MISSING,verbose, radiance_VL_SURF,reflectance_VL_SURF, ROT, BR, Q, U, rc, &
-                     albedo, reflectance_VL,radiance_VL, Q_lamb, U_lamb)
-  !
-  ! Place holder.
-  !
-     use VLIDORT_ScatMod
-   
-     implicit NONE
-
-    integer, parameter                      :: nkernel = 3   ! number of kernels
-    integer, parameter                      :: nparam  = 2   ! number of kernel parameters
-
-  ! !INPUT PARAMETERS:
-
-    integer,          intent(in)            :: km    ! number of vertical levels 
-    integer,          intent(in)            :: nch   ! number of channels
-    integer,          intent(in)            :: nobs  ! number of observations
-
-    integer, target,  intent(in)            :: nMom  ! number of phase function moments 
-    integer, target,  intent(in)            :: nPol  ! number of scattering matrix components                               
-                    
-    real*8, target,   intent(in)            :: channels(nch)    ! wavelengths [nm]
-
-  !                                                   ! --- Aerosol Optical Properties ---
-    real*8, target,   intent(in)            :: tau(km,nch,nobs) ! aerosol optical depth
-    real*8, target,   intent(in)            :: ssa(km,nch,nobs) ! single scattering albedo
-    real*8, target,   intent(in)            :: g(km,nch,nobs)   ! asymmetry factor
-    
-    real*8, target,   intent(in)            :: pmom(km,nMom,nPol,nch,nobs) !components of the scat phase matrix
-
-    real*8, target,   intent(in)            :: MISSING          ! MISSING VALUE
-    real*8, target,   intent(in)            :: pe(km+1,nobs)    ! pressure at layer edges [Pa]
-    real*8, target,   intent(in)            :: he(km+1,nobs)    ! height above sea-level  [m]
-    real*8, target,   intent(in)            :: te(km+1,nobs)    ! temperature at layer edges [K]
-
-    real*8, target,   intent(in)            :: kernel_wt(nkernel,nch,nobs)    ! kernel weights (/fiso,fgeo,fvol/)
-    real*8, target,   intent(in)            :: param(nparam,nch,nobs)         ! Li-Sparse parameters 
-                                                                              ! param1 = crown relative height (h/b)
-                                                                              ! param2 = shape parameter (b/r)
-                         
-    real*8, target,   intent(in)            :: solar_zenith(nobs)  
-    real*8, target,   intent(in)            :: relat_azymuth(nobs) 
-    real*8, target,   intent(in)            :: sensor_zenith(nobs) 
-
-    real*8, optional, target,   intent(in)  :: albedo(nobs,nch)               ! surface albedo optional
-
-    integer,          intent(in)            :: verbose
-
-  ! !OUTPUT PARAMETERS:
-    real*8,           intent(out)           :: radiance_VL_SURF(nobs,nch)     ! TOA normalized radiance from VLIDORT using surface module
-    real*8,           intent(out)           :: reflectance_VL_SURF(nobs, nch) ! TOA reflectance from VLIDORT using surface module
-    integer,          intent(out)           :: rc                             ! return code
-
-    real*8,           intent(out)           :: ROT(km,nobs,nch)               ! rayleigh optical thickness
-    real*8,           intent(out)           :: BR(nobs,nch)                   ! bidirectional reflectance 
-    real*8,           intent(out)           :: Q(nobs, nch)                   ! Stokes parameter Q
-    real*8,           intent(out)           :: U(nobs, nch)                   ! Stokes parameter U   
-
-    real*8, optional, intent(out)           :: reflectance_VL(nobs, nch)      ! TOA reflectance from VLIDORT using albedo
-    real*8, optional, intent(out)           :: radiance_VL(nobs,nch)          ! TOA normalized radiance from VLIDORT using albedo
-    real*8, optional, intent(out)           :: Q_lamb(nobs, nch)              ! Stokes parameter Q
-    real*8, optional, intent(out)           :: U_lamb(nobs, nch)              ! Stokes parameter U    
-  !                               ---
-    
-    integer             :: i,j,n,p,ier
-
-    
-    type(VLIDORT_scat) :: SCAT
-
-    rc = 0
-    ier = 0
-   
-    call VLIDORT_Init( SCAT%Surface%Base, km, rc)
-    if ( rc /= 0 ) return
-
-    SCAT%nMom = nMom
-    SCAT%nPol = nPol
-
-    do j = 1, nobs
-       
-      ! Make sure angles are available
-      ! -----------------------------------------
-      if ( IS_MISSING(solar_zenith(j),MISSING)  .OR. & 
-           IS_MISSING(sensor_zenith(j),MISSING) .OR. &
-           IS_MISSING(relat_azymuth(j),MISSING)  )  then
-
-        radiance_VL_SURF(j,:) = MISSING
-        reflectance_VL_SURF(j,:) = MISSING
-        
-        cycle
-
-      end if
-
-      SCAT%pe => pe(:,j)
-      SCAT%ze => he(:,j)
-      SCAT%te => te(:,j) 
-
-      do i = 1, nch
-        ! Make sure kernel weights and parameters are defined
-        do n = 1, nkernel
-          if (IS_MISSING(kernel_wt(n,i,j),MISSING)) then
-            radiance_VL_SURF(j,i) = MISSING
-            reflectance_VL_SURF(j,i) = MISSING
-            cycle
-          end if
-        end do
-        do p = 1, nparam
-          if (IS_MISSING(param(p,i,j),MISSING)) then
-            radiance_VL_SURF(j,i) = MISSING
-            reflectance_VL_SURF(j,i) = MISSING
-            cycle
-          end if
-        end do
-
-        if ( verbose > 0 ) then
-          print*, 'DO MODIS BRDF'
-        end if
-
-        call VLIDORT_LANDMODIS(SCAT%Surface,solar_zenith(j),&
-                               sensor_zenith(j),relat_azymuth(j),&
-                               kernel_wt(1,i,j),kernel_wt(2,i,j),kernel_wt(3,i,j),&
-                               reshape(param(:,i,j),(/nparam/)),&
-                               .true.,rc)
-
-        SCAT%wavelength = channels(i)        
-        SCAT%tau => tau(:,i,j)
-        SCAT%ssa => ssa(:,i,j)
-        SCAT%g => g(:,i,j)
-        SCAT%pmom => pmom(:,:,:,i,j)
-
-        call VLIDORT_Run (SCAT, radiance_VL_SURF(j,i),reflectance_VL_SURF(j,i),&
-                          ROT(:,j,i), Q(j,i), U(j,i), .false., .true., ier)
-
-        
-        BR(j,i) = SCAT%Surface%Base%VIO%VBRDF_Sup_Out%BS_DBOUNCE_BRDFUNC(1,1,1,1)
-        write(*,*) 'BR ',i,SCAT%Surface%Base%VIO%VBRDF_Sup_Out%BS_DBOUNCE_BRDFUNC(1,1,1,1)
-        if ( ier /= 0 ) then
-          radiance_VL_SURF(j,i) = MISSING
-          reflectance_VL_SURF(j,i) = MISSING               
-          cycle
-        end if
-
-        ! Mare sure albedo is defined
-        ! ---------------------------
-        if (present(albedo)) then
-          if ( verbose > 0 ) then
-            print *, 'albedo present'
-          end if 
-          if ( IS_MISSING(albedo(j,i),MISSING) ) then
-            radiance_VL(j,i) = MISSING
-            reflectance_VL(j,i) = MISSING
-            cycle
-          end if
-        else
-          if ( verbose > 0 ) then
-            print *, 'albedo not present'
-          end if
-          cycle
-        end if
-        
-        if ( verbose > 0 ) then
-          print*, 'DO SURFACE LAMB'
-        end if
-        call VLIDORT_SurfaceLamb(SCAT%Surface,albedo(j,i),solar_zenith (j),sensor_zenith(j),&
-                                 relat_azymuth(j),.true.)
-        if ( rc /= 0 ) return
-           
-        call VLIDORT_Run (SCAT, radiance_VL(j,i), reflectance_VL(j,i), &
-                          ROT(:,j,i), Q_lamb(j,i), U_lamb(j,i), .false., .true., ier)
- 
-        if ( ier /= 0 ) then
-          radiance_VL(j,i) = MISSING
-          reflectance_VL(j,i) = MISSING
-          cycle
-        end if
-      end do ! end loop over channels
-       
-      if ( verbose > 0 ) then
-        if ( mod(j-1,1000) == 0 ) then
-          print *, '<> VLIDORT Vector: ', nint(j*100./nobs), '%'
-        end if
-      end if
-
-    end do ! Loop over obs
-
-  end subroutine VLIDORT_Vector_LandMODIS
-
-end module VLIDORT_BRDF_MODIS
+end module LIDORT_BRDF_MODIS
