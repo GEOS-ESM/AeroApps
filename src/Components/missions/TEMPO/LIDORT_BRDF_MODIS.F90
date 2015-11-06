bc4ef08c877281e81d5e8dc37dccd135f8b654e6
Patricia Castellanos <patricia.castellanos@nasa.gov>
2015-11-06 17:19:03 -0500

    modified:   LIDORT_BRDF_MODIS.F90 	PC: now uses lidort_run_scalar and takes pmom as input

--- src/Components/missions/TEMPO/LIDORT_BRDF_MODIS.F90
+++ src/Components/missions/TEMPO/LIDORT_BRDF_MODIS.F90
@@ -18,11 +18,10 @@ module LIDORT_BRDF_MODIS
     return
   end function IS_MISSING
 
-  subroutine LIDORT_Scalar_LandMODIS (km, nch, nobs,channels,        &
-                     tau, ssa, g, pe, he, te, kernel_wt, param, &
+  subroutine LIDORT_Scalar_LandMODIS (km, nch, nobs,channels, nMom,       &
+                     nPol, tau, ssa, g, pmom, pe, he, te, kernel_wt, param, &
                      solar_zenith, relat_azymuth, sensor_zenith, &
-                     MISSING,verbose,radiance_L_SURF,reflectance_L_SURF, ROT, BR, rc, &
-                     albedo, reflectance_L,radiance_L )
+                     MISSING,verbose,radiance_L_SURF,reflectance_L_SURF, ROT, BR, rc )
   !
   ! Uses LIDORT in scalar mode to compute OMI aerosol TOA radiances.
   !
@@ -39,13 +38,17 @@ module LIDORT_BRDF_MODIS
     integer,          intent(in)  :: nch   ! number of channels
     integer,          intent(in)  :: nobs  ! number of observations
                                         
-                    
+    integer, target,  intent(in)  :: nMom             ! number of phase function moments     
+    integer, target,  intent(in)  :: nPol  ! number of scattering matrix components                               
+
     real*8, target,   intent(in)  :: channels(nch)    ! wavelengths [nm]
 
   !                                                   ! --- Aerosol Optical Properties ---
     real*8, target,   intent(in)  :: tau(km,nch,nobs) ! aerosol optical depth
     real*8, target,   intent(in)  :: ssa(km,nch,nobs) ! single scattering albedo
     real*8, target,   intent(in)  :: g(km,nch,nobs)   ! asymmetry factor
+    real*8, target,   intent(in)  :: pmom(km,nch,nobs,nMom,nPol) !components of the scat phase matrix
+
 
 
     real*8, target,   intent(in)  :: pe(km+1,nobs)    ! pressure at layer edges [Pa]
@@ -61,8 +64,6 @@ module LIDORT_BRDF_MODIS
     real*8, target,   intent(in)  :: solar_zenith(nobs)  
     real*8, target,   intent(in)  :: relat_azymuth(nobs) 
     real*8, target,   intent(in)  :: sensor_zenith(nobs) 
-
-    real*8, optional, target,   intent(in)  :: albedo(nobs,nch)       ! surface albedo optional
     
     integer,          intent(in)            :: verbose
 
@@ -73,21 +74,20 @@ module LIDORT_BRDF_MODIS
     integer,          intent(out) :: rc                               ! return code
     real*8,           intent(out) :: ROT(km,nobs,nch)                 ! rayleigh optical thickness  
     real*8,           intent(out) :: BR(nobs,nch)                     ! bi-directional reflectance 
-    real*8, optional, intent(out) :: reflectance_L(nobs, nch)        ! TOA reflectance from LIDORT using albedo
-    real*8, optional, intent(out) :: radiance_L(nobs,nch)            ! TOA normalized radiance from LIDORT using albedo
 
-  !  real*8,           intent(out) :: BRDF(nobs, nch)  
-  !                         ---  
     integer             :: i,j,n,p,ier
 
   
-    type(LIDORT_scat) :: SCAT
+    type(LIDORT_scat)           :: SCAT
+    type(LIDORT_output_scalar)  :: output
 
     rc = 0
     ier = 0
 
     call LIDORT_Init( SCAT%Surface%Base, km, rc)
     if ( rc /= 0 ) return
+
+    SCAT%nMom    = nMom
 
     do j = 1,nobs
 
@@ -97,8 +97,8 @@ module LIDORT_BRDF_MODIS
            IS_MISSING(sensor_zenith(j),MISSING) .OR. &
            IS_MISSING(relat_azymuth(j),MISSING)  )  then
 
-        radiance_L(j,:) = MISSING
-        reflectance_L(j,:) = MISSING
+        radiance_L_SURF(j,:) = MISSING
+        reflectance_L_SURF(j,:) = MISSING
         cycle
       end if
         
@@ -139,59 +139,33 @@ module LIDORT_BRDF_MODIS
         SCAT%tau => tau(:,i,j)
         SCAT%ssa => ssa(:,i,j)
         SCAT%g => g(:,i,j)
+        SCAT%pmom => pmom(:,i,j,:,:)
          
-        call LIDORT_Run (SCAT, radiance_L_SURF(j,i), reflectance_L_SURF(j,i), &
-                          ROT(:,j,i), aerosol, ier)
+        call LIDORT_Run_Scalar (SCAT, output, ier)
 
+        radiance_L_SURF(j,i)    = output%radiance
+        reflectance_L_SURF(j,i) = output%reflectance
         BR(j,i) = SCAT%Surface%Base%VIO%BRDF_Sup_Out%BS_DBOUNCE_BRDFUNC(1,1,1)
+        ROT(:,j,i) = SCAT%rot   
 
         if ( verbose > 0 ) then
           print *, 'My radiance land modis',radiance_L_SURF(j,i), reflectance_L_SURF(j,i) 
         end if
 
         if ( ier /= 0 ) then
-          radiance_L(j,i) = MISSING
-          reflectance_L(j,i) = MISSING
+          radiance_L_SURF(j,i) = MISSING
+          reflectance_L_SURF(j,i) = MISSING
           cycle
         end if
 
-        if ( verbose > 0 ) then
-          print *, 'now check for albedo'       
-        end if
+        if (BR(j,i) < 0) then
+          radiance_L_SURF(j,i)    = -500
+          reflectance_L_SURF(j,i) = -500
+          BR(j,i)    = -500
+          ROT(:,j,i) = -500
+          cycle
+        end if        
 
-        ! Mare sure albedo is defined
-        ! ---------------------------
-        if (present(albedo)) then
-          if ( verbose > 0 ) then
-            print *, 'albedo present'
-        end if 
-        if ( IS_MISSING(albedo(j,i),MISSING) ) then
-          radiance_L(j,i) = MISSING
-          reflectance_L(j,i) = MISSING
-          cycle
-        end if
-        else
-          if ( verbose > 0 ) then
-            print *, 'albedo not present'
-          end if
-          cycle
-        end if
-        if ( verbose > 0 ) then
-          print*, 'DO SURFACE LAMB'
-        end if
-        call LIDORT_SurfaceLamb(SCAT%Surface,albedo(j,i),solar_zenith (j),sensor_zenith(j),&
-                               relat_azymuth(j))
-         
-        call LIDORT_Run (SCAT, radiance_L(j,i), reflectance_L(j,i), &
-                        ROT(:,j,i), aerosol, ier)
-        if ( verbose > 0 ) then
-          print *, 'radiance albedo',albedo(j,i),radiance_L(j,i), reflectance_L(j,i) 
-        end if
-        if ( ier /= 0 ) then
-          radiance_L(j,i) = MISSING
-          reflectance_L(j,i) = MISSING
-          cycle
-        end if
       end do ! end loop over channels
      
       if ( verbose > 0 ) then
