724466dff0fb25669d4012fc25fb500376052a15
Patricia Castellanos <patricia.castellanos@nasa.gov>
2015-12-10 10:55:51 -0500

    new file:   ../SENTINEL-4/Aod_EOS.rc 	new file:   ../SENTINEL-4/Chem_MieRegistry.rc 	new file:   ../SENTINEL-4/GNUmakefile 	new file:   ../SENTINEL-4/clean_mem.sh 	new file:   ../SENTINEL-4/geo_angles.F90 	new file:   ../SENTINEL-4/run_geo_angles_batch.py 	new file:   ../SENTINEL-4/run_geo_sampler_batch.py     PC: initial commit of files to do SENTINEL-4 Runs

---
+++ src/Components/missions/SENTINEL-4/run_geo_angles_batch.py
@@ -0,0 +1,77 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+""" Runscript for geo_angles"""
+from datetime import datetime, timedelta 
+from dateutil.parser import parse
+import os
+import subprocess
+from distutils.dir_util import mkpath
+import numpy as np
+import math
+
+
+
+def make_rcfile(indir,outdir,date,instname,layout=None):
+
+    dirname = outdir+'/LevelB/Y'+str(date.year)+'/M'+str(date.month).zfill(2)+'/D'+str(date.day).zfill(2)
+    if not os.path.exists(dirname):
+        os.makedirs(dirname)
+
+    rcfile = open('geo_angles.rc','w')
+    rcfile.write('INDIR: ' + indir + '\n')
+    rcfile.write('OUTDIR: ' + outdir + '\n')
+    rcfile.write('DATE: '+str(date.year)+str(date.month).zfill(2)+str(date.day).zfill(2)+'\n')
+    rcfile.write('TIME: '+str(date.hour).zfill(2)+'\n')
+    rcfile.write('INSTNAME: ' + instname + '\n')
+    if layout is not None:
+        rcfile.write('LAYOUT: ' + layout + '\n')
+    rcfile.close()    
+        
+#########################################################
+
+if __name__ == "__main__":
+    
+    startdate   = '2005-12-31T00:00:00'
+    enddate     = '2005-12-31T23:00:00'
+    layout      = None
+    episode     = None
+    instname    = 'sentinel-4'
+    calculon    = '/nobackup/SENTINEL-4'
+
+    if type(episode) is int: episode = [episode]
+    if episode is None: episode = [None]
+
+    for e in episode:
+        if e == 1:
+            startdate  = '2005-12-31T00:00:00Z'
+            enddate    = '2006-01-01T23:00:00Z'
+        elif e == 2:
+            startdate  = '2006-07-27T00:00:00Z'
+            enddate    = '2006-08-09T23:00:00Z'
+        elif e == 3:
+            startdate  = '2007-03-28T00:00:00Z'
+            enddate    = '2007-03-29T23:00:00Z'
+        elif e == 4:
+            startdate  = '2007-04-10T00:00:00Z'
+            enddate    = '2007-04-11T23:00:00Z'
+    
+        indir     = calculon 
+        outdir    = calculon
+        
+        dt = timedelta(hours=1)
+        startdate = parse(startdate)
+        enddate   = parse(enddate)
+
+        # Create RC-files
+
+        while (startdate <= enddate):
+            make_rcfile(indir,outdir,startdate,instname,layout=layout)
+
+            runstring = 'mpirun -np 8 ./geo_angles.x geo_angles.rc'
+            os.system(runstring)
+
+            startdate = startdate + dt
+
+    
+
+
