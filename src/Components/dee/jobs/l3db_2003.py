555f5c938022464b30eb29bf91dc90590fb560c6
Arlindo M. da Silva, Jr <arlindo.dasilva@nasa.gov>
2016-01-25 20:15:27 -0500

    ams: work in progress

---
+++ src/Components/dee/jobs/l3db_2003.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+"""
+Select dee blue dust for a range of years.
+"""
+import sys
+sys.path.insert(1,'../')
+
+from deep import *
+
+#....................................................................
+
+if __name__ == "__main__":
+
+    path = '/nobackup/MODIS/006/Level2'
+    years = (2003, 2005)
+
+    grid_dust(path,years[0],years[1],dirn='../DEE')
+    
