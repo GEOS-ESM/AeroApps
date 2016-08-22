ad61c12e4396e32b3eab45f5016cef995c4310e0
Patricia Castellanos <patricia.castellanos@nasa.gov>
2016-08-22 17:24:54 -0400

    PC: working on fill_values

--- src/Components/missions/MODIS/leo_sampler.py
+++ src/Components/missions/MODIS/leo_sampler.py
@@ -393,26 +393,26 @@ def writeNC ( mxd, Vars, levs, levUnits, options,
         # _copyVar(ncGeo,nc,u'ew',dtype='f4',zlib=False)
         # _copyVar(ncGeo,nc,u'ns',dtype='f4',zlib=False)
 
-        dt = nc.createVariable('scanTime','f4',('cell_along_swath','cell_across_swath',),zlib=False)
+        dt = nc.createVariable('scanTime','f4',('cell_along_swath','cell_across_swath',),
+                                fill_value=MISSING,zlib=False)
         dt.long_name     = 'Time of Scan'
         dt.units         = 'seconds since %s'%DATE_START.isoformat(' ')
-        dt._FillValue    = MISSING
         dt[:]            = mxd.Scan_Start_Time[i]
 
         
         # Save lon/lat if so desired
         # --------------------------
         if options.coords:
-            clon = nc.createVariable('clon','f4',('cell_along_swath','cell_across_swath',),zlib=False)
+            clon = nc.createVariable('clon','f4',('cell_along_swath','cell_across_swath',),
+                                     fill_value=MISSING,zlib=False)
             clon.long_name     = 'pixel center longitude'
             clon.missing_value = MISSING
-            clon._FillValue    = MISSING
             clon[:]            = mxd.Longitude[i]
 
-            clat = nc.createVariable('clon','f4',('cell_along_swath','cell_across_swath',),zlib=False)
+            clat = nc.createVariable('clon','f4',('cell_along_swath','cell_across_swath',),
+                                     fill_value=MISSING,zlib=False)
             clat.long_name     = 'pixel center latitude'
             clat.missing_value = MISSING
-            clat._FillValue    = MISSING
             clat[:]            = mxd.Latitude[i]
 
 
