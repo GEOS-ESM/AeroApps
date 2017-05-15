3d6466a1b170b1f8fe0c2164e75bfd7edc47ab08
Patricia Castellanos <patricia.castellanos@nasa.gov>
2017-05-15 11:13:12 -0400

    PC: work in progress

--- src/Components/lidar_vlidort/VLIDORT_LIDAR_py.F90
+++ src/Components/lidar_vlidort/VLIDORT_LIDAR_py.F90
@@ -6,12 +6,12 @@
 !  May 2017
 !.............................................................................
 
-subroutine VECTOR_BRDF_MODIS(km, nch, nobs, channels, nMom,  &
-                     nPol,tau, ssa, pmom, pe, he, te, kernel_wt, param, &
+subroutine VECTOR_BRDF_MODIS(km, nch, nobs, channels, nMom, nPol,nkernel,nparam, &
+                     tau, ssa, pmom, pe, he, te, kernel_wt, param, &
                      solar_zenith, relat_azymuth, sensor_zenith, &
-                     MISSING,verbose, radiance_VL_SURF,reflectance_VL_SURF, ROT, BR, Q, U, rc, DO_2OS_CORRECTION)
+                     MISSING,verbose, radiance_VL_SURF,reflectance_VL_SURF, ROT, BR, Q, U, rc)
 
-    use VLIDORT_BRDF_MODIS  
+    use VLIDORT_BRDF_MODIS, only: VLIDORT_Vector_LandMODIS  
     implicit None
 
   ! !INPUT PARAMETERS:
@@ -20,29 +20,32 @@ subroutine VECTOR_BRDF_MODIS(km, nch, nobs, channels, nMom,  &
     integer,          intent(in)            :: nch   ! number of channels
     integer,          intent(in)            :: nobs  ! number of observations
 
-    integer, target,  intent(in)            :: nMom  ! number of phase function moments 
-    integer, target,  intent(in)            :: nPol  ! number of scattering matrix components                               
+    integer,          intent(in)            :: nMom  ! number of phase function moments 
+    integer,          intent(in)            :: nPol  ! number of scattering matrix components                               
+
+    integer,          intent(in)            :: nkernel ! number of kernels
+    integer,          intent(in)            :: nparam  ! number of kernel parameters
                     
-    real*8, target,   intent(in)            :: channels(nch)    ! wavelengths [nm]
+    real*8,           intent(in)            :: channels(nch)    ! wavelengths [nm]
 
   !                                                   ! --- Aerosol Optical Properties ---
-    real*8, target,   intent(in)            :: tau(km,nch,nobs) ! aerosol optical depth
-    real*8, target,   intent(in)            :: ssa(km,nch,nobs) ! single scattering albedo    
-    real*8, target,   intent(in)            :: pmom(km,nch,nobs,nMom,nPol) !components of the scat phase matrix
+    real*8,           intent(in)            :: tau(km,nch,nobs) ! aerosol optical depth
+    real*8,           intent(in)            :: ssa(km,nch,nobs) ! single scattering albedo    
+    real*8,           intent(in)            :: pmom(km,nch,nobs,nMom,nPol) !components of the scat phase matrix
 
-    real*8, target,   intent(in)            :: MISSING          ! MISSING VALUE
-    real*8, target,   intent(in)            :: pe(km+1,nobs)    ! pressure at layer edges [Pa]
-    real*8, target,   intent(in)            :: he(km+1,nobs)    ! height above sea-level  [m]
-    real*8, target,   intent(in)            :: te(km+1,nobs)    ! temperature at layer edges [K]
+    real*8,           intent(in)            :: MISSING          ! MISSING VALUE
+    real*8,           intent(in)            :: pe(km+1,nobs)    ! pressure at layer edges [Pa]
+    real*8,           intent(in)            :: he(km+1,nobs)    ! height above sea-level  [m]
+    real*8,           intent(in)            :: te(km+1,nobs)    ! temperature at layer edges [K]
 
-    real*8, target,   intent(in)            :: kernel_wt(nkernel,nch,nobs)    ! kernel weights (/fiso,fgeo,fvol/)
-    real*8, target,   intent(in)            :: param(nparam,nch,nobs)         ! Li-Sparse parameters 
+    real*8,           intent(in)            :: kernel_wt(nkernel,nch,nobs)    ! kernel weights (/fiso,fgeo,fvol/)
+    real*8,           intent(in)            :: param(nparam,nch,nobs)         ! Li-Sparse parameters 
                                                                               ! param1 = crown relative height (h/b)
                                                                               ! param2 = shape parameter (b/r)
                          
-    real*8, target,   intent(in)            :: solar_zenith(nobs)  
-    real*8, target,   intent(in)            :: relat_azymuth(nobs) 
-    real*8, target,   intent(in)            :: sensor_zenith(nobs) 
+    real*8,           intent(in)            :: solar_zenith(nobs)  
+    real*8,           intent(in)            :: relat_azymuth(nobs) 
+    real*8,           intent(in)            :: sensor_zenith(nobs) 
 
     integer,          intent(in)            :: verbose
 
@@ -58,15 +61,15 @@ subroutine VECTOR_BRDF_MODIS(km, nch, nobs, channels, nMom,  &
 
 
     call VLIDORT_Vector_LandMODIS (km, nch, nobs, channels, nMom, &
-                                   nPol, tau, ssa, pmom, pe, ze, te, &
+                                   nPol, tau, ssa, pmom, pe, he, te, &
                                    kernel_wt, param, &
                                    solar_zenith, &
                                    relat_azymuth, &
                                    sensor_zenith, &
-                                   MISSING,verbose,
-                                   radiance_VL_int,
-                                   reflectance_VL_int, 
-                                   ROT, albedo, Q, U, ierr )  
+                                   MISSING,verbose, &
+                                   radiance_VL_SURF, &
+                                   reflectance_VL_SURF, &
+                                   ROT, BR, Q, U, rc )  
 
 
 end subroutine VECTOR_BRDF_MODIS
