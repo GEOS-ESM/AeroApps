00badd428e45b1c20c04af6c775b1ce5988b58c6
Patricia Castellanos <patricia.castellanos@nasa.gov>
2017-05-16 12:48:41 -0400

    PC: working version

--- src/Components/lidar_vlidort/mcd43c.py
+++ src/Components/lidar_vlidort/mcd43c.py
@@ -12,7 +12,7 @@ from   glob                   import glob
 from   scipy.interpolate      import RegularGridInterpolator
 from   netCDF4                import Dataset
 
-
+nkernels = 3
 SDS = { 'BRDF_Albedo_Parameter1_vis'                 : 'Risovis',
         'BRDF_Albedo_Parameter1_nir'                 : 'Risonir',
         'BRDF_Albedo_Parameter2_vis'                 : 'Rvolvis',
@@ -39,7 +39,34 @@ SDS = { 'BRDF_Albedo_Parameter1_vis'                 : 'Risovis',
         'BRDF_Albedo_Parameter3_Band4'               : 'Rgeo550',
         'BRDF_Albedo_Parameter3_Band5'               : 'Rgeo1200',
         'BRDF_Albedo_Parameter3_Band6'               : 'Rgeo1600',
-        'BRDF_Albedo_Parameter3_Band7'               : 'Rgeo2100'}       
+        'BRDF_Albedo_Parameter3_Band7'               : 'Rgeo2100'}   
+
+#----
+def _copyVar(ncIn,ncOut,name,dtype='f4',zlib=False,verbose=False):
+    """
+    Create variable *name* in output file and copy its
+    content over,
+    """
+    x = ncIn.variables[name]
+    if verbose:
+        print 'copy variable ',name,x.dimensions
+    y = ncOut.createVariable(name,dtype,x.dimensions,zlib=zlib)
+    if hasattr(x,'long_name'): y.long_name = x.long_name
+    if hasattr(x,'units'): y.units = x.units 
+    try:
+        y.missing_value = x.missing_value
+    except:
+        pass
+    rank = len(x.shape)
+
+    if rank == 1:
+        y[:] = x[:]
+    elif rank == 2:
+        y[:,:] = x[:,:]
+    elif rank == 3:
+        y[:,:,:] = x[:,:,:]
+    else:
+        raise ValueError, "invalid rank of <%s>: %d"%(name,rank)
 
 class BRDF(object):
     def __init__(self,nobs,omp=False):
@@ -53,7 +80,8 @@ class BRDF(object):
 
 
 class MCD43C(object):
-    def __init__(self,datadir):
+    def __init__(self,datadir,verbose=False):
+        self.verbose = verbose
         self.dlon = 0.05
         self.dlat = 0.05
 
@@ -105,6 +133,68 @@ class MCD43C(object):
         
         return inFileList[0]
 
+    #----
+    def writenc(self,nctrj,outFile,verbose=False):
+        """
+        Write a NetCDF file with sampled MCD43C1 kernel weights on lidar trajectory
+        """
+
+        # Dimensions
+        # ------------------
+        ntime = len(self.tyme)
+
+        # Open NC file
+        # ------------
+        nc = Dataset(outFile,'w',format='NETCDF4_CLASSIC')
+
+        # Set global attributes
+        # ---------------------
+        nc.title = "MCD43C1 Trajectory Sampler"
+        nc.institution = 'NASA/Goddard Space Flight Center'
+        nc.source = 'Global Model and Assimilation Office'
+        nc.history = 'Created from MCD43C1 v006 collections by mcd43c_sampler.py'
+        nc.references = 'n/a'
+        nc.comment = 'This file contains BRDF Kernels weights for the RTLS model for 8 MODIS bands sampled on a satellite trajectory'
+        nc.contact = 'Patricia Castellanos <patricia.castellanos@nasa.gov>'
+        nc.Conventions = 'CF'    
+        nc.BAND1 = "620-670nm"
+        nc.BAND2 = "841-875nm"
+        nc.BAND3 = "459-479nm"
+        nc.BAND4 = "545-565nm"
+        nc.BAND5 = "1230-1250nm"
+        nc.BAND6 = "1628-1652nm"
+        nc.BAND7 = "2105-2155nm"
+
+        # Create dimensions
+        # -----------------
+        t = nc.createDimension('time',ntime)
+        s = nc.createDimension('ls',19)
+        x = nc.createDimension('x',1)  
+        y = nc.createDimension('y',1)
+
+        # Save lon/lat
+        # --------------------------
+        _copyVar(nctrj,nc,u'trjLon',dtype='f4',zlib=False,verbose=verbose)
+        _copyVar(nctrj,nc,u'trjLat',dtype='f4',zlib=False,verbose=verbose)
+        _copyVar(nctrj,nc,u'time', dtype='i4',zlib=False,verbose=verbose)
+        _copyVar(nctrj,nc,u'isotime', dtype='S1',zlib=False,verbose=verbose)
+        _copyVar(nctrj,nc,u'x',dtype='f4',zlib=False,verbose=verbose)
+        _copyVar(nctrj,nc,u'y',dtype='f4',zlib=False,verbose=verbose)            
+
+        # Loop over Bands writing each dataset
+        #---------------------------------------
+        dim = ('time',)
+        for sds in SDS:
+          this = nc.createVariable(SDS[sds],'f4',dim)  
+
+          this.long_name = SDS[sds][4:] + ' BRDF Kernel weight'
+          this.missing_value = -99999
+          this.unit = 'none'  
+          this[:] = self.brdf.__dict__[SDS[sds]]
+
+
+        nc.close()          
+
     def sample(self,inFile,outFile,Verbose=False,omp=False):
         if omp:
             import pymp
@@ -125,11 +215,15 @@ class MCD43C(object):
         trjLon = nctrj.variables['trjLon'][:]
 
         # Round dates to day
-        dtyme = np.array([parse(str(d.date())) for d in tyme])
+        dtyme = np.array([isoparser(str(d.date())) for d in tyme])
         utyme = np.sort(np.unique(dtyme))
 
 
-        giant.brdf = BRDF(len(tyme),omp=omp)
+        self.brdf = BRDF(len(tyme),omp=omp)
+        self.tyme = tyme
+        self.trjLon = trjLon
+        self.trjLat = trjLat
+
 
         if omp:
             # use openmp type parallel processing
@@ -149,7 +243,7 @@ class MCD43C(object):
 
                     for sds in SDS:
                         interpFunc = RegularGridInterpolator((self.lat, self.lon), self.__dict__[SDS[sds]],method='nearest')
-                        giant.brdf.__dict__[SDS[sds]][Ityme] = interpFunc(pts)
+                        self.brdf.__dict__[SDS[sds]][Ityme] = interpFunc(pts)
         else:
             for ut in utyme:
                 if Verbose:
@@ -166,5 +260,9 @@ class MCD43C(object):
 
                 for sds in SDS:
                     interpFunc = RegularGridInterpolator((self.lat, self.lon), self.__dict__[SDS[sds]],method='nearest')
-                    giant.brdf.__dict__[SDS[sds]][Ityme] = interpFunc(pts)            
+                    self.brdf.__dict__[SDS[sds]][Ityme] = interpFunc(pts)      
+
+
+        self.writenc(nctrj,outFile,verbose=Verbose) 
+        nctrj.close()     
 
