d17312b0a4ee5027dc0b176bc8dc583be0a249a3
Patricia Castellanos <patricia.castellanos@nasa.gov>
2017-05-18 10:45:52 -0400

    PC: copyvar is now a separate function

--- src/Components/lidar_vlidort/mcd43c.py
+++ src/Components/lidar_vlidort/mcd43c.py
@@ -11,6 +11,7 @@ import numpy                  as     np
 from   glob                   import glob
 from   scipy.interpolate      import RegularGridInterpolator
 from   netCDF4                import Dataset
+from   copyvar                import _copyVar
 
 nkernels = 3
 SDS = { 'BRDF_Albedo_Parameter1_vis'                 : 'Risovis',
@@ -41,32 +42,6 @@ SDS = { 'BRDF_Albedo_Parameter1_vis'                 : 'Risovis',
         'BRDF_Albedo_Parameter3_Band6'               : 'Rgeo1600',
         'BRDF_Albedo_Parameter3_Band7'               : 'Rgeo2100'}   
 
-#----
-def _copyVar(ncIn,ncOut,name,dtype='f4',zlib=False,verbose=False):
-    """
-    Create variable *name* in output file and copy its
-    content over,
-    """
-    x = ncIn.variables[name]
-    if verbose:
-        print 'copy variable ',name,x.dimensions
-    y = ncOut.createVariable(name,dtype,x.dimensions,zlib=zlib)
-    if hasattr(x,'long_name'): y.long_name = x.long_name
-    if hasattr(x,'units'): y.units = x.units 
-    try:
-        y.missing_value = x.missing_value
-    except:
-        pass
-    rank = len(x.shape)
-
-    if rank == 1:
-        y[:] = x[:]
-    elif rank == 2:
-        y[:,:] = x[:,:]
-    elif rank == 3:
-        y[:,:,:] = x[:,:,:]
-    else:
-        raise ValueError, "invalid rank of <%s>: %d"%(name,rank)
 
 class BRDF(object):
     def __init__(self,nobs,omp=False):
