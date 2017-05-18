997fa854f8229d08be53db00a460c80173c941f8
Patricia Castellanos <patricia.castellanos@nasa.gov>
2017-05-18 10:46:21 -0400

    PC: work in progress.

--- src/Components/lidar_vlidort/polar_vlidort.py
+++ src/Components/lidar_vlidort/polar_vlidort.py
@@ -21,6 +21,7 @@ from dateutil.parser import parse         as isoparser
 from MAPL.constants import *
 import LidarAngles_    
 import VLIDORT_LIDAR_ 
+from copyvar  import _copyVar
 
 # Generic Lists of Varnames and Units
 VNAMES_DU = ['DU001','DU002','DU003','DU004','DU005']
@@ -355,151 +356,137 @@ class POLAR_VLIDORT(object):
                                                       MISSING,
                                                       self.verbose)        
 
-            #radiance_VL_SURF,reflectance_VL_SURF, ROT, BR, Q, U, rc = result
         
+        self.writenc(result)
+        
+    #---
+    def writeNC (self,result,zlib=True):
+        """
+        Write a NetCDF file vlidort output
+        """
+        radiance_VL_SURF,reflectance_VL_SURF, ROT, BR, Q, U, rc = result
+        km = 72
 
+        # Open NC file
+        # ------------
+        nc = Dataset(self.outFile,'w',format='NETCDF4_CLASSIC')
 
-        return result
-
-
-
-#---
-def writeNC ( stations, lons, lats, tyme, isotimeIn, MieVars, MieVarsNames,
-              MieVarsUnits, inFile, outFile, zlib=False):
-    """
-    Write a NetCDF file with sampled GEOS-5 variables along the satellite track
-    described by (lon,lat,tyme).
-    """
-    km = 72
-
-    # Open NC file
-    # ------------
-    nc = Dataset(outFile,'w',format=options.format)
-
-    # Set global attributes
-    # ---------------------
-    nc.title = 'GEOS-5 Sampled Aerosol Optical Properties File'
-    nc.institution = 'NASA/Goddard Space Flight Center'
-    nc.source = 'Global Model and Assimilation Office'
-    nc.history = 'Created from sampled GEOS-5 collections'
-    nc.references = 'n/a'
-    nc.comment = 'This file contains sampled GEOS-5 aerosol optical properties.'
-    nc.contact = 'Ed Nowottnick <edward.p.nowottnick@nasa.gov>'
-    nc.Conventions = 'CF'
-    nc.inFile = inFile
- 
-    # Create dimensions
-    # -----------------
-    nt = nc.createDimension('time',len(tyme))
-    if options.station:
-        ns = nc.createDimension('station',len(stations))
-    ls = nc.createDimension('ls',19)
-    if km>0:
+        # Set global attributes
+        # ---------------------
+        nc.title = 'VLIDORT Simulation of GEOS-5 multiangle polarized reflectance'
+        nc.institution = 'NASA/Goddard Space Flight Center'
+        nc.source = 'Global Model and Assimilation Office'
+        nc.history = 'VLIDORT simulation run on sampled GEOS-5'
+        nc.references = 'n/a'
+        nc.contact = 'Patricia Castellanos <patricia.castellanos@nasa.gov>'
+        nc.Conventions = 'CF'
+        nc.inFile = self.inFile
+     
+        # Create dimensions
+        # -----------------
+        nt = nc.createDimension('time',len(self.tyme))
+        ls = nc.createDimension('ls',19)
         nz = nc.createDimension('lev',km)
-    x = nc.createDimension('x',1)
-    y = nc.createDimension('y',1)
+        x  = nc.createDimension('x',1)
+        y  = nc.createDimension('y',1)
+        na = nc.createDimension('view_angles',2*len(self.VZA))
 
-    if options.station:
-        # Station names
-        # -------------
-        stnName_ = nc.createVariable('stnName','S1',('station','ls'),zlib=zlib)
-        stnName_.long_name = 'Station Names'
-        stnName_.axis = 'e'
-        stnName_[:] = stations[:]   
+        # Coordinate variables
+        # --------------------
+        col = 'aer_Nv'
+        if self.verbose: 
+            print 'opening file',self.inFile.replace('%col',col)
+        nctrj       = Dataset(self.inFile.replace('%col',col))        
+        _copyVar(nctrj,nc,u'trjLon',dtype='f4',zlib=False,verbose=verbose)
+        _copyVar(nctrj,nc,u'trjLat',dtype='f4',zlib=False,verbose=verbose)
+        _copyVar(nctrj,nc,u'time', dtype='i4',zlib=False,verbose=verbose)
+        _copyVar(nctrj,nc,u'isotime', dtype='S1',zlib=False,verbose=verbose)
+        _copyVar(nctrj,nc,u'x',dtype='f4',zlib=False,verbose=verbose)
+        _copyVar(nctrj,nc,u'y',dtype='f4',zlib=False,verbose=verbose)   
+        nctrj.close()
 
-    # Coordinate variables
-    # --------------------
-    time = nc.createVariable('time','i4',('time',),zlib=zlib)
-    time.long_name = 'Time'
-    t0 = tyme[0]
-    isot0 = isotimeIn[0]
-    date0 = ''.join(isot0[:10])
-    time0 = ''.join(isot0[-8:])
-    time.units = 'seconds since '+date0+' '+time0
-    time[:] = tyme
-    if km > 0: # pressure level not supported yet
-        lev = nc.createVariable('lev','f4',('lev',),zlib=zlib)
-        lev.long_name = 'Vertical Level'
-        lev.units = 'km'
-        lev.positive = 'down'
-        lev.axis = 'z'
-        lev[:] = range(1,km+1)
 
-    # Add fake dimensions for GrADS compatibility
-    # -------------------------------------------
-    x = nc.createVariable('x','f4',('x',),zlib=zlib)
-    x.long_name = 'Fake Longitude for GrADS Compatibility'
-    x.units = 'degrees_east'
-    x[:] = zeros(1)
-    y = nc.createVariable('y','f4',('y',),zlib=zlib)
-    y.long_name = 'Fake Latitude for GrADS Compatibility'
-    y.units = 'degrees_north'
-    y[:] = zeros(1)
-    if options.station:
-        e = nc.createVariable('station','i4',('station',),zlib=zlib)
-        e.long_name = 'Station Ensemble Dimension'
-        e.axis = 'e'
-        e.grads_dim = 'e'
-        e[:] = range(len(stations))
-    
-    # Lat/Lon Coordinates
-    # ----------------------
-    if options.station:
-        lon = nc.createVariable('longitude','f4',('station',),zlib=zlib)
-        lon.long_name = 'Longitude'
-        lon.units = 'degrees_east'
-        lon[:] = lons[:]
-        lat = nc.createVariable('latitude','f4',('station',),zlib=zlib)
-        lat.long_name = 'Latitude'
-        lat.units = 'degrees_north'
-        lat[:] = lats[:]
-    else:
-        lon = nc.createVariable('longitude','f4',('time',),zlib=zlib)
-        lon.long_name = 'Longitude'
-        lon.units = 'degrees_east'
-        lon[:] = lons[:]
-        lat = nc.createVariable('latitude','f4',('time',),zlib=zlib)
-        lat.long_name = 'Latitude'
-        lat.units = 'degrees_north'
-        lat[:] = lats[:]        
-    
-    # Time in ISO format if so desired
-    # ---------------------------------
-    isotime = nc.createVariable('isotime','S1',('time','ls'),zlib=zlib)
-    isotime.long_name = 'Time (ISO Format)'
-    isotime[:] = isotimeIn[:]
+        vza = nc.createVariable('seonsor_zenith','f4',('time','view_angles',),zlib=zlib)
+        vza.long_name = "sensor viewing zenith angle (VZA)"
+        vza.missing_value = MISSING
+        vza.units = "degrees (positive forward view)"
 
-    # Write each variable
-    # --------------------------------------------------
-    for n, name in enumerate(MieVarsNames):
+        vaa = nc.createVariable('sensor_azimuth','f4',('time','view_angles',),zlib=zlib)
+        vaa.long_name = "sensor viewing azimuth angle (VAA)"
+        vaa.missing_value = MISSING
+        vaa.units = "degrees clockwise from North"
 
-        var = squeeze(MieVars[name])
-        size = len(var.shape)
-        if options.station:
+        sza = nc.createVariable('seonsor_zenith','f4',('time',),zlib=zlib)
+        sza.long_name = "solar zenith angle (SZA)"
+        sza.missing_value = MISSING
+        sza.units = "degrees"
+
+        saa = nc.createVariable('solar_azimuth','f4',('time',),zlib=zlib)
+        saa.long_name = "solar azimuth angle (SAA)"
+        saa.missing_value = MISSING
+        saa.units = "degrees clockwise from North"
+
+
+        # Write VLIDORT Outputs
+        # ---------------------
+        ref = nc.createVariable('I','f4',('time','view_angles',),zlib=zlib)
+        ref.standard_name = '%.2f nm TOA Reflectance' %self.channel
+        ref.long_name     = '%.2f nm Top of the Atmosphere Reflectance' %self.channel
+        ref.missing_value = MISSING
+        ref.units         = "None"
+        ref._FillValue    = MISSING
+
+        q = nc.createVariable('I','f4',('time','view_angles',),zlib=zlib)
+        q.standard_name = '%.2f nm TOA Reflectance' %self.channel
+        q.long_name     = '%.2f nm Top of the Atmosphere Reflectance' %self.channel
+        q.missing_value = MISSING
+        q.units         = "None"
+        q._FillValue    = MISSING        
+
+        sref = nc.createVariable('surf_reaf','f4',('time','view_angles',),zlib=zlib)
+        ref.standard_name = '%.2f nm Surface Reflectance' %self.channel
+        ref.long_name     = '%.2f nm Bi-Directional Surface Reflectance' %self.channel
+        ref.missing_value = MISSING
+        ref.units         = "None"
+        ref._FillValue    = MISSING
+
+        rot = nc.createVariable('ROT','f4',('time',),zlib=zlib)
+        rot.long_name = '%.2f nm Rayleigh Optical Thickness' %self.channel
+        rot.missing_value = MISSING
+        rot.units         = "None"
+        rot._FillValue    = MISSING
+
+
+
+        for n, name in enumerate(MieVarsNames):
+
+            var = squeeze(MieVars[name])
+            size = len(var.shape)
+            if options.station:
+                if size == 2:
+                    var = asarray([var])
+                    size = len(var.shape)
+            if size == 3:
+                dim = ('station','time','lev')
             if size == 2:
-                var = asarray([var])
-                size = len(var.shape)
-        if size == 3:
-            dim = ('station','time','lev')
-        if size == 2:
-            dim = ('time','lev')
-        if size == 1:
-            dim = ('time')
-        this = nc.createVariable(name,'f4',dim,zlib=zlib)
-        this.standard_name = name
-        this.units = MieVarsUnits[n]
-        this.missing_value = MAPL_UNDEF
-        if options.station:
-            this[:] = transpose(var,(0,2,1))
-        else:
-            this[:] = transpose(var)
+                dim = ('time','lev')
+            if size == 1:
+                dim = ('time')
+            this = nc.createVariable(name,'f4',dim,zlib=zlib)
+            this.standard_name = name
+            this.units = MieVarsUnits[n]
+            this.missing_value = MAPL_UNDEF
+            if options.station:
+                this[:] = transpose(var,(0,2,1))
+            else:
+                this[:] = transpose(var)
 
-    # Close the file
-    # --------------
-    nc.close()
+        # Close the file
+        # --------------
+        nc.close()
 
-    if options.verbose:
-        print " <> wrote %s file %s"%(options.format,options.outFile)
+        if options.verbose:
+            print " <> wrote %s file %s"%(options.format,options.outFile)
     
 
 def get_chd(channel):
@@ -541,7 +528,7 @@ if __name__ == "__main__":
 
    
     # Run ext_sampler
-    vlidort.runExt()
+    #vlidort.runExt()
 
     # Run VLIDORT
     # if vlidort.nobs > 0:
