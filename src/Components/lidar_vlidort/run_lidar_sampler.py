71c952c106aca95f6c644d2ce1c3b8c78bcea09f
Patricia Castellanos <patricia.castellanos@nasa.gov>
2017-06-05 16:43:41 -0400

    PC: filelist init in coll loop

--- src/Components/lidar_vlidort/run_lidar_sampler.py
+++ src/Components/lidar_vlidort/run_lidar_sampler.py
@@ -105,11 +105,11 @@ if __name__ == "__main__":
         # run trajectory sampler on model fields
         # split across multiple processors by date
         datelist = [Date + p*pdt for p in range(args.nproc)]
-        filelist = []
-
+        
         # run trajectory sampler on model fields
         for rc,colname in zip(rcFiles,colNames):
             processes = set()
+            filelist = []
             for date in datelist:
                 nymd = str(date.date()).replace('-','')
                 hour = str(date.hour).zfill(2)
