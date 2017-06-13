1247fe9a32afa76971bca2f30ac7107fec05d24b
Patricia Castellanos <patricia.castellanos@nasa.gov>
2017-06-13 10:00:25 -0400

    PC handle lambertian surfaces

--- src/Components/lidar_vlidort/VLIDORT_LIDAR_py.F90
+++ src/Components/lidar_vlidort/VLIDORT_LIDAR_py.F90
@@ -146,3 +146,64 @@ subroutine VECTOR_BRDF_MODIS_BPDF(km, nch, nobs, channels, nMom, nPol,nkernel,np
 
 
 end subroutine VECTOR_BRDF_MODIS_BPDF
+
+subroutine VECTOR_LAMBERT(km, nch, nobs, channels, nMom, nPol, &
+                     tau, ssa, pmom, pe, he, te, albedo, &
+                     solar_zenith, relat_azymuth, sensor_zenith, &
+                     MISSING,verbose, radiance_VL_SURF,reflectance_VL_SURF, ROT, Q, U, rc)
+
+    use VLIDORT_LAMBERT, only: VLIDORT_Vector_Lambert  
+    implicit None
+
+  ! !INPUT PARAMETERS:
+
+    integer,          intent(in)            :: km    ! number of vertical levels 
+    integer,          intent(in)            :: nch   ! number of channels
+    integer,          intent(in)            :: nobs  ! number of observations
+
+    integer,          intent(in)            :: nMom  ! number of phase function moments 
+    integer,          intent(in)            :: nPol  ! number of scattering matrix components                               
+                    
+    real*8,           intent(in)            :: channels(nch)    ! wavelengths [nm]
+
+  !                                                   ! --- Aerosol Optical Properties ---
+    real*8,           intent(in)            :: tau(km,nch,nobs) ! aerosol optical depth
+    real*8,           intent(in)            :: ssa(km,nch,nobs) ! single scattering albedo    
+    real*8,           intent(in)            :: pmom(km,nch,nobs,nMom,nPol) !components of the scat phase matrix
+
+    real*8,           intent(in)            :: MISSING          ! MISSING VALUE
+    real*8,           intent(in)            :: pe(km+1,nobs)    ! pressure at layer edges [Pa]
+    real*8,           intent(in)            :: he(km+1,nobs)    ! height above sea-level  [m]
+    real*8,           intent(in)            :: te(km+1,nobs)    ! temperature at layer edges [K]
+
+    real*8, target,   intent(in)            :: albedo(nobs,nch)       ! surface albedo
+                         
+    real*8,           intent(in)            :: solar_zenith(nobs)  
+    real*8,           intent(in)            :: relat_azymuth(nobs) 
+    real*8,           intent(in)            :: sensor_zenith(nobs) 
+
+    integer,          intent(in)            :: verbose
+
+  ! !OUTPUT PARAMETERS:
+    real*8,           intent(out)           :: radiance_VL_SURF(nobs,nch)     ! TOA normalized radiance from VLIDORT using surface module
+    real*8,           intent(out)           :: reflectance_VL_SURF(nobs, nch) ! TOA reflectance from VLIDORT using surface module
+    integer,          intent(out)           :: rc                             ! return code
+
+    real*8,           intent(out)           :: ROT(km,nobs,nch)               ! rayleigh optical thickness
+    real*8,           intent(out)           :: Q(nobs, nch)                   ! Stokes parameter Q
+    real*8,           intent(out)           :: U(nobs, nch)                   ! Stokes parameter U   
+
+
+    call VLIDORT_Vector_Lambert (km, nch, nobs, channels, nMom, &
+                                   nPol, tau, ssa, pmom, pe, he, te, &
+                                   albedo, &
+                                   solar_zenith, &
+                                   relat_azymuth, &
+                                   sensor_zenith, &
+                                   MISSING,verbose, &
+                                   radiance_VL_SURF, &
+                                   reflectance_VL_SURF, &
+                                   ROT, Q, U, rc )  
+
+
+end subroutine VECTOR_LAMBERT
