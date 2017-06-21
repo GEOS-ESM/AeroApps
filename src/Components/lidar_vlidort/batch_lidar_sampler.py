ac0dd4169819187b5df2a048c4bd4800f9162b00
Patricia Castellanos <patricia.castellanos@nasa.gov>
2017-06-21 11:52:17 -0400

    PC - dt_hours should be a passed option

--- src/Components/lidar_vlidort/batch_lidar_sampler.py
+++ src/Components/lidar_vlidort/batch_lidar_sampler.py
@@ -59,7 +59,7 @@ if __name__ == '__main__':
 
         iso1 = Date.isoformat()
         iso2 = edate.isoformat()
-        newline = 'python -u run_lidar_sampler.py -v --nproc 6 --DT_hours 24 {} {} {} >'.format(iso1,iso2,args.prep_config) + ' slurm_${SLURM_JOBID}_py.out\n'
+        newline = 'python -u run_lidar_sampler.py -v --nproc 6 --DT_hours {} {} {} {} >'.format(args.DT_hours,iso1,iso2,args.prep_config) + ' slurm_${SLURM_JOBID}_py.out\n'
         text[-2] = newline
         f.close()
 
