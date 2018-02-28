76f77f7140b08b4d63676379c6fde4ff0cb9567e
Patricia Castellanos <patricia.castellanos@nasa.gov>
2018-02-28 13:24:58 -0500

    PC - wrappers for BRDF sampling

---
+++ src/Components/missions/AERONET/aeronet_mcd43c_sampler.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+"""
+Wrapper to call mcd43c_stn_sampler.py
+"""
+
+import os
+import argparse
+
+#------------------------------------ M A I N ------------------------------------
+
+if __name__ == "__main__":
+    # Defaults
+    DT_hours = 1
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("iso_t1",
+                        help="starting iso time")
+    parser.add_argument("iso_t2",
+                        help="ending iso time")
+
+    parser.add_argument("prep_config",
+                        help="prep config filename")
+
+    parser.add_argument("-D","--DT_hours", default=DT_hours, type=int,
+                        help="Timestep in hours for each file (default=%i)"%DT_hours)
+
+    parser.add_argument("-v", "--verbose",action="store_true",
+                        help="Verbose mode (default=False).")    
+
+
+    args = parser.parse_args()
+
+    command = "mcd43c_stn_sampler.py"
+
+    command += ' -D {}'.format(args.DT_hours)
+    if args.verbose:
+        command += ' -v'
+
+    command += ' {} {} {}'.format(args.iso_t1,args.iso_t2,args.prep_config)
+
+    print command
+    os.system(command)
