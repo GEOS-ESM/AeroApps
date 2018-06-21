c2e9789ea4ec38001810a390a2825a1f752adf67
Patricia Castellanos <patricia.castellanos@nasa.gov>
2018-06-21 13:33:37 -0400

    PC - wrapper scripts for AI calculations

---
+++ src/Components/geo_vlidort/run_geo_vlidort_AI_lc2.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# -W ignore::DeprecationWarning
+
+"""
+
+  Simple wrapper script to parse Prep config file and 
+  call geo_vlidort_lc2.py
+  
+  May 2017
+  patricia.castellanos@nasa.gov
+"""
+
+from os       import system
+from optparse import OptionParser
+from MAPL     import Config
+
+if __name__ == "__main__":
+    
+#   Parse command line options
+#   --------------------------
+    parser = OptionParser(usage="Usage: %prog prep_config_file start_isotime end_isotime")
+    parser.add_option("-n", "--dryrun",
+                      action="store_true", dest="dryrun",
+                      help="Dry run.")
+
+    (options, args) = parser.parse_args()
+    
+    if len(args) == 3:
+        prep_config, start_isotime, end_isotime = args
+    else:
+        parser.error("must have 3 arguments: prep_config_filename start_isotime end_isotime")
+
+
+    # Parse prep config
+    # -----------------
+    cf = Config(prep_config,delim=' = ')
+ 
+    Options =           " --instname=" + cf('LC2_INSTNAME')      + \
+                  " --version_string=" + cf('LC2_VERSION')       + \
+                        " --channels=" + cf('LC2_CHANNELS')     + \
+                         " --nodemax=" + cf('LC2_NODEMAX')      + \
+                          " --layout=" + cf('LC2_LAYOUT')       + \
+                          " --cldmax=" + cf('LC2_CLDMAX')       + \
+                             " --dir=" + cf('LC2_DIR')          + \
+                         " --runmode=" + cf('LC2_RUNMODE')       + \
+                         " --runfile=" + cf('LC2_RUNFILE')      + \
+                          " --execfile=" + cf('LC2_EXECFILE') 
+
+    # Optional
+    try:
+        Options += " --c_band=" + cf('LC2_SURF_C_BAND')                      
+    except:
+        pass
+
+    if cf('LC2_PROFILE').upper()     == 'YES': Options += " --profile"
+    if cf('LC2_VERBOSE').upper()     == 'YES': Options += " --verbose"
+    if cf('LC2_KEEP_LB').upper()     == 'YES': Options += " --keep_lb"
+    if cf('LC2_ARCHIVE_LB').upper()  == 'YES': Options += " --archive_lb"
+
+    # Generate LC2 products
+    # ---------------------
+    cmd = "geo_vlidort_lc2_AI.py %s %s %s "%(Options,start_isotime,end_isotime)
+    print cmd
+    if not options.dryrun:
+        if system(cmd):
+            raise ValueError, "geo_vlidort_lc2_AI.py failed for %s to %s "%(start_isotime, end_isotime)
+
+    
+
+
+
+
---
+++ src/Components/missions/TEMPO/tempo_AI_lc2.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# -W ignore::DeprecationWarning
+
+"""
+
+  Simple wrapper script to run different episodes of TEMPO swath using
+  run_geo_vlidort_lc2.py
+  
+  May 2017.
+  patricia.castellanos@nasa.gov
+"""
+from os       import system
+from optparse        import OptionParser
+
+if __name__ == "__main__":
+    bin = 'run_geo_vlidort_lc2_AI.py'
+
+#   Parse command line options
+#   --------------------------
+    parser = OptionParser(usage="Usage: %prog [OPTIONS] pcfFile",
+                          version='1.0.0' )
+
+    parser.add_option("-b", "--bin", dest="bin",default=bin, 
+              help="executable for run (default=%s)" %bin ) 
+
+    parser.add_option("-s", "--isoStartTime", dest="isoStartTime", 
+              help="start isotime" ) 
+
+    parser.add_option("-e", "--isoEndTime", dest="isoEndTime", 
+              help="end isotime" )                  
+
+    parser.add_option("-E", "--episode", dest="episode",
+              help="preset episode number" )
+
+    parser.add_option("-d", "--dryrun",
+                      action="store_true", dest="dryrun",
+                      help="do a dry run")        
+
+    # Parse arguments
+    # ---------------
+    (options, args) = parser.parse_args()
+    if len(args) == 1:
+        pcfFile = args[0]
+    else:
+        parser.error("must have 1 arguments: pcfFile")    
+
+    isoStartTime = options.isoStartTime
+    isoEndTime   = options.isoEndTime
+    episode      = options.episode
+    dryrun       = options.dryrun
+    bin          = options.bin
+
+    if episode is not None:
+        if int(episode) == 1:
+            isoStartTime  = '2005-12-31T00:00:00Z'
+            isoEndTime    = '2006-01-01T23:00:00Z'
+        elif int(episode) == 2:
+            isoStartTime  = '2006-07-27T00:00:00Z'
+            isoEndTime    = '2006-08-09T23:00:00Z'
+        elif int(episode) == 3:
+            isoStartTime  = '2007-03-28T00:00:00Z'
+            isoEndTime    = '2007-03-29T23:00:00Z'
+        elif int(episode) == 4:
+            isoStartTime  = '2007-04-10T00:00:00Z'
+            isoEndTime    = '2007-04-11T23:00:00Z'
+
+    if dryrun:
+        command = bin + ' --dryrun'
+    else:
+        command = bin
+
+    command += ' ' + pcfFile + ' ' + isoStartTime + ' ' + isoEndTime
+
+    print command
+    if system(command):
+        raise ValueError, "run_geo_vlidort_lc2_AI.py failed for %s to %s "%(isoStartTime,isoEndTime)
