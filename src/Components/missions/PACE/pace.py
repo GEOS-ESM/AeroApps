8d5cfae049a08536109ce63ec8f68e281734f271
Patricia Castellanos <patricia.castellanos@nasa.gov>
2018-09-07 15:58:59 -0400

    PC - objects to read levelB files for cloud simulation

--- src/Components/missions/PACE/pace.py
+++ src/Components/missions/PACE/pace.py
@@ -132,6 +132,80 @@ class PACE(object):
                 v.mask = np.zeros(v.shape).astype('bool')
             self.__dict__[sds].append(v) 
 
+
+class LEVELBCS(PACE):
+    """ Read in PACE LevelB data for the cloud simulator"""
+    def __init__(self,Path, SDS,verb=False):
+        # Read one granule
+        # ---------------------------------------------
+        if type(Path) is list:
+            if len(Path) == 0:
+               raise Exception("WARNING: Empty PACE_LEVELB path")
+        else:
+           Path = [Path, ]
+
+        # Create empty lists for SDS to be read from file
+        # -----------------------------------------------
+        self.granules = Path
+        self.verb = verb
+        self.SDS  = SDS
+
+        for name in self.SDS:
+           self.__dict__[name] = []
+
+        self.scanStart = []
+
+        # Read each granule, appending them to the list
+        # ---------------------------------------------
+        self._readList(Path)
+
+        # Alias
+        for sds in self.SDS:
+            if sds in ALIAS:
+                self.__dict__[ALIAS[sds]] = self.__dict__[sds]
+
+        # Create corresponding python time
+        # --------------------------------
+        if hasattr(self,'midTime'):
+            nscan,npixel = self.lon[0].shape
+            self.tyme = []        
+            for scanStart,midTime,lon in zip(self.scanStart,self.midTime,self.lon):
+                scanStart  = isoparser(scanStart.strftime('2006-%m-%dT00:00:00'))
+                tyme       = np.array([scanStart + timedelta(seconds=int(t)) for t in midTime])    
+                tyme.shape = (nscan,1)
+                tyme       = np.repeat(tyme,npixel,axis=1)
+                tyme       = np.ma.array(tyme)
+                tyme.mask  = lon.mask
+                self.tyme.append(tyme)
+
+#---
+    def _readGranule(self,filename):
+        """Reads one PACE LevelB granule."""
+
+        # Exit if the file cannot be opened
+        # ---------------------------------------
+        try:
+            if self.verb:
+                print "[] Working on "+filename
+            nc = Dataset(filename)
+        except:
+            raise Exception("- %s: not recognized as an netCDF file"%filename)
+
+        # Read select variables (do not reshape)
+        # --------------------------------------
+        if len(self.scanStart) == 0:
+            self.scanStart.append(isoparser(nc.time_coverage_start))
+        for sds in self.SDS:            
+            if len(self.__dict__[sds]) == 0:
+                # Don't fuss if you can't find it
+                try:
+                    v = nc.variables[sds][:]
+                    if not hasattr(v,'mask'):
+                        v = np.ma.array(v)
+                        v.mask = np.zeros(v.shape).astype('bool')
+                    self.__dict__[sds].append(v) 
+                except:
+                    pass
             
 # ---
 def granules ( path, t1, t2):
@@ -144,7 +218,7 @@ def granules ( path, t1, t2):
     t2        ---  ending time (timedate format)
     """
 
-    # Find MODIS granules in the time range
+    # Find PACE granules in the time range
     # ------------------------------------------
     dt = timedelta(minutes=5)
     t = datetime(t1.year,t1.month,t1.day,t1.hour,0,0)
@@ -168,7 +242,81 @@ def granules ( path, t1, t2):
 
     return Granules
 
+# ---
+def granulesLB ( path, t1, t2, coll):
+    """
+    Returns a list of PACE LevelB granules for a given product at given time.
+    On input,
 
+    path      ---  mounting point for the PACE files
+    t1        ---  starting time (timedate format)
+    t2        ---  ending time (timedate format)
+    coll      ---  GEOS data collection
+    """
+
+    # Find PACE granules in the time range
+    # ------------------------------------------
+    if type(coll) is str:
+        coll = [coll]
+    dt = timedelta(minutes=5)
+    Granules = []
+    for collname in coll:
+        t = datetime(t1.year,t1.month,t1.day,t1.hour,0,0)
+        while t <= t2:
+            if t >= t1:
+                basen = "%s/Y%04d/M%02d/D%02d/pace-g5nr.lb.%s.%04d%02d%02d_%02d%02d00.nc4"\
+                         %(path,t.year,t.month,t.day,collname,t.year,t.month,t.day,t.hour,t.minute)
+                
+                try:
+                    filen = glob(basen)[0]
+                    Granules += [filen,]
+    #               print " [x] Found "+filen
+                except:
+                    pass
+            t += dt
+
+    if len(Granules) == 0:
+        print "WARNING: no PACE LevelB granules found for %s through %s"%(str(t1), str(t2))
+
+    return Granules
+
+# ---
+def granulesLBN ( path, t1, t2, coll):
+    """
+    Returns a list of PACE LevelB granules for a given product at given time.
+    On input,
+
+    path      ---  mounting point for the PACE files
+    t1        ---  starting time (timedate format)
+    t2        ---  ending time (timedate format)
+    coll      ---  GEOS data collection
+    """
+
+    # Find PACE granules in the time range
+    # ------------------------------------------
+    if type(coll) is str:
+        coll = [coll]
+    dt = timedelta(minutes=5)    
+    Granules = []
+    for collname in coll:
+        t = datetime(t1.year,t1.month,t1.day,t1.hour,0,0)
+        while t <= t2:
+            if t >= t1:
+                basen = "%s/Y%04d/M%02d/D%02d/pace-g5nr.lb-nearest.%s.%04d%02d%02d_%02d%02d00.nc4"\
+                         %(path,t.year,t.month,t.day,collname,t.year,t.month,t.day,t.hour,t.minute)
+                
+                try:
+                    filen = glob(basen)[0]
+                    Granules += [filen,]
+    #               print " [x] Found "+filen
+                except:
+                    pass
+            t += dt
+
+    if len(Granules) == 0:
+        print "WARNING: no PACE LevelB granules found for %s through %s"%(str(t1), str(t2))
+
+    return Granules
     
 #------------------------------------ M A I N ------------------------------------
 
