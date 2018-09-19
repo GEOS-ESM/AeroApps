68d4b9e320cd859530522fd7c7aef1234f7363f8
Patricia Castellanos <patricia.castellanos@nasa.gov>
2018-09-19 16:36:59 -0400

    PC = additional interfaces for different size arrays

--- src/Components/leo_vlidort/READ_PAR_py.F90
+++ src/Components/leo_vlidort/READ_PAR_py.F90
@@ -6,6 +6,40 @@
 !  May 2017
 !.............................................................................
 
+subroutine py_mp_readvar1D(varname, filename, var)
+
+    use mp_netcdf_Mod, only: mp_readvar1d  
+    implicit None
+
+  ! !INPUT PARAMETERS:
+
+    character(len=*), intent(in)           ::  varname
+    character(len=*), intent(in)           ::  filename
+    real*8, dimension(:), intent(inout)      ::  var
+
+
+    call mp_readvar1d (varname, filename, var )  
+
+
+end subroutine py_mp_readvar1D
+
+subroutine py_mp_readvar2D(varname, filename, var)
+
+    use mp_netcdf_Mod, only: mp_readvar2d  
+    implicit None
+
+  ! !INPUT PARAMETERS:
+
+    character(len=*), intent(in)           ::  varname
+    character(len=*), intent(in)           ::  filename
+    real*8, dimension(:,:), intent(inout)    ::  var
+
+
+    call mp_readvar2d (varname, filename, var )  
+
+
+end subroutine py_mp_readvar2D
+
 subroutine py_mp_readvar3D(varname, filename, var)
 
     use mp_netcdf_Mod, only: mp_readvar3d  
@@ -23,4 +57,20 @@ subroutine py_mp_readvar3D(varname, filename, var)
 
 end subroutine py_mp_readvar3D
 
+subroutine py_mp_readvar4D(varname, filename, var)
+
+    use mp_netcdf_Mod, only: mp_readvar4d  
+    implicit None
+
+  ! !INPUT PARAMETERS:
+
+    character(len=*), intent(in)             ::  varname
+    character(len=*), intent(in)             ::  filename
+    real, dimension(:,:,:,:), intent(inout)  ::  var
+
+
+    call mp_readvar4d (varname, filename, var )  
+
+
+end subroutine py_mp_readvar4D
 
