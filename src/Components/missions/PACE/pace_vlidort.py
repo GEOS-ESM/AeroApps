a6a575cab6a5a1b00b85c3e009e313f930a8dee1
Patricia Castellanos <patricia.castellanos@nasa.gov>
2018-11-07 17:03:38 -0500

    PC -attempt to write main pave simulator as a python script.

--- src/Components/missions/PACE/pace_vlidort.py
+++ src/Components/missions/PACE/pace_vlidort.py
@@ -16,11 +16,12 @@ import os
 from   netCDF4 import Dataset
 import numpy   as np
 
+from   mieobs  import  getAOPvector, getAOPint, getEdgeVars
 from datetime        import datetime, timedelta
 from dateutil.parser import parse         as isoparser
 
 from   MAPL  import config
-from   leo_vlidort import LEO_VLIDORT, get_chd, SDS_AER, SDS_CLD, SDS_INV, nMom
+from   leo_vlidort import LEO_VLIDORT, get_chd, SDS_AER, SDS_CLD, SDS_WIND, AERNAMES, MIENAMES
 from   pace  import PACE, LEVELBCS
 import multiprocessing
 import READ_NC_
@@ -38,51 +39,88 @@ class HOLDER(PACE):
         pass
 
 
+class MieVARS(object):
+    """
+    container for mie vars calculations
+    """
+    pass
+
+
+def unwrap_doMie(arg, **kwarg):
+    return doMie(*arg, **kwarg)
+
+def doMie(AERDATA,channel,rcFile,nMom,t):
+    NAMES = AERNAMES + ['PS','DELP','RH','AIRDENS']
+    inVars = MieVARS()
+    for i,sds in enumerate(NAMES):
+        var = AERDATA[i]
+        if len(var.shape) ==2:
+            v = var[:,t]
+            v.shape = v.shape + (1,)
+            inVars.__dict__[sds] = v
+        elif len(var.shape) ==1:
+            v = np.array(var[t])
+            v.shape = (1,)
+            inVars.__dict__[sds] = v
+
+    tau,ssa,g,pmom = getAOPvector(inVars,channel,
+                             vnames=AERNAMES,
+                             Verbose=False,
+                             rcfile=rcFile,
+                             nMom=nMom)
+
+    vol, area, refr, refi, reff = getAOPint(inVars,channel,
+                             vnames=AERNAMES,
+                             Verbose=False,
+                             rcfile=rcFile)
+
+    return tau,ssa,g,pmom,refr,refi,vol
+
 class PACE_VLIDORT(LEO_VLIDORT,LEVELBCS,PACE):
     """
     Everything needed for calling VLIDORT
     Inherit methods from LEO_VLIDORT
-    GEOS-5 has already been sampled on lidar track
+    GEOS-5 has already been sampled on LEO track
     """
-    def __init__(self,LGinFile,LBinFile,LCinFile,outFile,rcFile,albedoType,
-                channel,
-                brdfFile=None,
-                ndviFile=None,
-                lcFile=None,
-                lerFile=None,
-                verbose=False,
-                extOnly=False,
-                distOnly=False,
-                outFileDist=None,
-                pool=None):
+    # def __init__(self,LGinFile,LBinFile,LCinFile,outFile,rcFile,albedoType,
+    #             channel,
+    #             brdfFile=None,
+    #             ndviFile=None,
+    #             lcFile=None,
+    #             lerFile=None,
+    #             verbose=False,
+    #             extOnly=False,
+    #             distOnly=False,
+    #             outFileDist=None,
+    #             pool=None):
 
-        self.SDS_AER      = SDS_AER
-        self.SDS_CLD      = SDS_CLD
-        self.SDS_INV      = SDS_INV
-        self.SDS_GEOM     = SDS_GEOM
-        self.LGinFile     = LGinFile
-        self.LBinFile     = LBinFile
-        self.LCinFile     = LCinFile
-        self.outFile      = outFile
-        self.outFileDist  = outFileDist
-        self.albedoType   = albedoType
-        self.rcFile       = rcFile
-        self.channel      = channel
-        self.verbose      = verbose
-        self.nMom         = nMom
-        self.brdfFile     = brdfFile
-        self.lcFile       = lcFile
-        self.ndviFile     = ndviFile
-        self.lerFile      = lerFile
+    #     self.SDS_AER      = SDS_AER
+    #     self.SDS_CLD      = SDS_CLD
+    #     self.SDS_INV      = SDS_INV
+    #     self.SDS_GEOM     = SDS_GEOM
+    #     self.LGinFile     = LGinFile
+    #     self.LBinFile     = LBinFile
+    #     self.LCinFile     = LCinFile
+    #     self.outFile      = outFile
+    #     self.outFileDist  = outFileDist
+    #     self.albedoType   = albedoType
+    #     self.rcFile       = rcFile
+    #     self.channel      = channel
+    #     self.verbose      = verbose
+    #     self.nMom         = nMom
+    #     self.brdfFile     = brdfFile
+    #     self.lcFile       = lcFile
+    #     self.ndviFile     = ndviFile
+    #     self.lerFile      = lerFile
 
-        if not extOnly:
-            # initialize empty lists
-            for sds in self.SDS_AER+self.SDS_CLD+self.SDS_INV:
-                self.__dict__[sds] = []
+    #     if not extOnly:
+    #         # initialize empty lists
+    #         for sds in self.SDS_AER+self.SDS_CLD+self.SDS_INV:
+    #             self.__dict__[sds] = []
 
-            # Read in geometry and model data
-            self.readGeom()
-            self.readSampledGEOS()
+    #         # Read in geometry and model data
+    #         self.readGeom()
+    #         self.readSampledGEOS()
 
 
     #---
@@ -134,9 +172,8 @@ class PACE_VLIDORT(LEO_VLIDORT,LEVELBCS,PACE):
                 v = np.squeeze(var).T
                 v = v[~self.offview3d]
                 v = v.reshape([self.km,self.nobs])
-                v = v.T
 
-            self.__dict__[sds] = v  #(nobs,nz)
+            self.__dict__[sds] = v  #(nz,nobs)
 
             if self.verbose: 
                 print 'Read ',sds
@@ -152,19 +189,43 @@ class PACE_VLIDORT(LEO_VLIDORT,LEVELBCS,PACE):
             v = np.squeeze(var).T
             v = v[~self.offview3d]
             v = v.reshape([self.km,self.nobs])
-            v = v.T
+
+            self.__dict__[sds] = v  #(nz,nobs)
+
+            if self.verbose: 
+                print 'Read ',sds
+
+
+    #---
+    def read10mWind(self):
+        """
+        Read in model sampled track wind data
+        """
+        col = 'met_Nv'
+        self.SDS_WIND = SDS_WIND
+        METFile = self.LBinFile.replace('%col',col)
+        if self.verbose: 
+            print 'opening file',METFile
+ 
+        for sds in self.SDS_WIND:
+            var = np.zeros([self.shape[1],self.shape[0],1],dtype=np.float32,order='F')
+            READ_NC_.py_readvar3d(sds,METFile,var)
+            v = np.squeeze(var).T
+            v = v[~self.offview]
 
             self.__dict__[sds] = v  #(nobs,nz)
 
             if self.verbose: 
                 print 'Read ',sds
-
               
 
 
 #------------------------------------ M A I N ------------------------------------
 
 if __name__ == "__main__":
+    # # start fork here so large data is not copied to children
+    nproc = int(multiprocessing.cpu_count()*0.5)    
+    pool = multiprocessing.Pool(nproc) 
 
     date     = datetime(2006,03,24,00,50)
     gdate    = datetime(2020,date.month,date.day,date.hour,date.minute)
@@ -187,16 +248,14 @@ if __name__ == "__main__":
     ndviFile     = '{}/calipso-g5nr.lb2.ndvi.{}_{}z.nc4'.format(ndviDir,nymd,hour)
     lcDir        = '/nobackup/3/pcastell/POLAR_LIDAR/CALIPSO/BPDF/LAND_COVER/MCD12C1/051/Y{}/M{}'.format(date.year,str(date.month).zfill(2))
     lcFile       = '{}/calipso-g5nr.lb2.land_cover.{}_{}z.nc4'.format(lcDir,nymd,hour)    
-    albedoType   = 'MODIS_BRDF_BPDF'
+    albedoType   = 'CX'
 
     channel  = 470
     chd      = get_chd(channel)
-    outDir    = '/nobackup/3/pcastell/POLAR_LIDAR/CALIPSO/LevelC/Y{}/M{}'.format(date.year,str(date.month).zfill(2))
-    outFile   = '{}/calipso-g5nr.vlidort.vector.MCD43C.{}_{}z_{}nm.nc4'.format(outDir,nymd,hour,chd)
+    outDir    = '/nobackup/PACE/LevelC/Y{}/M{}'.format(date.year,str(date.month).zfill(2))
+    outFile   = '{}/pace-g5nr.vlidort.vector.CX.{}_{}_{}nm.nc4'.format(outDir,nymd,hhmm,chd)
     
     rcFile   = 'Aod_EOS.rc'
-    VZAname  = 'POLDER'
-    orbit    = 'LEO'
     verbose  = True
 
     # Initialize VLIDORT class getting aerosol optical properties
@@ -208,7 +267,54 @@ if __name__ == "__main__":
                             ndviFile=ndviFile,
                             lcFile=lcFile,
                             verbose=verbose,
-                            distOnly=True)
+                            distOnly=False)
+
+    if not v.extOnly:
+        # Calculate aerosol optical properties
+
+        #doMie(AERDATA,channel,rcFile,nMom,t)
+
+        NAMES = AERNAMES + ['PS','DELP','RH','AIRDENS']
+        nbatch = 10
+
+
+        #for sii in np.arange(0,v.nobs,nbatch): 
+        for sii in np.arange(1):   
+            eii = sii + nbatch 
+            nobs = nbatch
+            if eii > v.nobs:
+                eii = v.nobs
+                nobs = eii - sii
+
+            # for sds in MIENAMES:
+            #     if sds == 'pmom':
+            #         v.__dict__[sds] = np.zeros([v.km,1,nobs,v.nMom,4])
+            #     else:
+            #         v.__dict__[sds] = np.zeros([v.km,1,nobs]) #(km,nch,nobs)
+
+            
+            args = HOLDER()
+            argsList = []
+            for sds in NAMES:
+                var = v.__dict__[sds]
+            
+                if len(var.shape) == 2:
+                    args.__dict__[sds] = var[:,sii:eii]
+                else:
+                    args.__dict__[sds] = var[sii:eii]
+
+                argsList.append(args.__dict__[sds])
+
+            argsALL = zip(nobs*[argsList],nobs*[v.channel],nobs*[v.rcFile],nobs*[v.nMom],range(nobs))
+            result = pool.map(unwrap_doMie,argsALL)
+            # for r in result:
+            #     # tau, ssa, g, pmom,refi,refr,vol = r
+            #     for sds,miedata in zip(MIENAMES,r):
+            #         v.__dict__[sds] = miedata
+            #         # if sds == 'pmom':
+            #         #     v.__dict__[sds][:,:,sii:eii,:,:] = miedata
+            #         # else:
+            #         #     v.__dict__[sds][:,:,sii:eii] = miedata
 
    
     # Run ext_sampler
@@ -217,4 +323,7 @@ if __name__ == "__main__":
     # Run VLIDORT
     # if vlidort.nobs > 0:
     #     vlidort.runVLIDORT()
+
+    pool.close()
+    pool.join()
 
