d47e9d5f70c23e9d3518b34fdb5d44516bfa7ca9
Patricia Castellanos <patricia.castellanos@nasa.gov>
2019-03-20 15:43:50 -0400

    PC - new python interface for lambert_bpdf option

--- src/Components/leo_vlidort/VLIDORT_POLAR_py.F90
+++ src/Components/leo_vlidort/VLIDORT_POLAR_py.F90
@@ -283,6 +283,77 @@ subroutine VECTOR_LAMBERT(km, nch, nobs, channels, nMom, nPol, &
 
 end subroutine VECTOR_LAMBERT
 
+subroutine VECTOR_LAMBERT_BPDF(km, nch, nobs, channels, nMom, nPol, nparam, &
+                     tau, ssa, pmom, pe, he, te, albedo, BPDFparam, &
+                     solar_zenith, relat_azymuth, sensor_zenith, &
+                     MISSING,verbose, radiance_VL_SURF,reflectance_VL_SURF, BR, ROT, Q, U, BR_Q, BR_U, rc)
+
+    use VLIDORT_LAMBERT_BPDF, only: VLIDORT_Vector_Lambert_BPDF  
+    implicit None
+
+  ! !INPUT PARAMETERS:
+
+    integer,          intent(in)            :: km    ! number of vertical levels 
+    integer,          intent(in)            :: nch   ! number of channels
+    integer,          intent(in)            :: nobs  ! number of observations
+
+    integer,          intent(in)            :: nMom  ! number of phase function moments 
+    integer,          intent(in)            :: nPol  ! number of scattering matrix components                               
+
+    integer,          intent(in)            :: nparam  ! number of kernel parameters
+                     
+    real*8,           intent(in)            :: channels(nch)    ! wavelengths [nm]
+
+  !                                                   ! --- Aerosol Optical Properties ---
+    real*8,           intent(in)            :: tau(km,nch,nobs) ! aerosol optical depth
+    real*8,           intent(in)            :: ssa(km,nch,nobs) ! single scattering albedo    
+    real*8,           intent(in)            :: pmom(km,nch,nobs,nMom,nPol) !components of the scat phase matrix
+
+    real*8,           intent(in)            :: MISSING          ! MISSING VALUE
+    real*8,           intent(in)            :: pe(km+1,nobs)    ! pressure at layer edges [Pa]
+    real*8,           intent(in)            :: he(km+1,nobs)    ! height above sea-level  [m]
+    real*8,           intent(in)            :: te(km+1,nobs)    ! temperature at layer edges [K]
+
+    real*8, target,   intent(in)            :: albedo(nobs,nch)       ! surface albedo
+    real*8,           intent(in)            :: BPDFparam(nparam,nch,nobs)     ! BPRDF parameters 
+                                                                              ! param1 = Refractive index of water (1.5)
+                                                                              ! param2 = NDVI , must be <=1 or >=1
+                                                                              ! param3 = Scaling factor (C, from Maignan 2009 RSE Table 1)                         
+                         
+    real*8,           intent(in)            :: solar_zenith(nobs)  
+    real*8,           intent(in)            :: relat_azymuth(nobs) 
+    real*8,           intent(in)            :: sensor_zenith(nobs) 
+
+    integer,          intent(in)            :: verbose
+
+  ! !OUTPUT PARAMETERS:
+    real*8,           intent(out)           :: radiance_VL_SURF(nobs,nch)     ! TOA normalized radiance from VLIDORT using surface module
+    real*8,           intent(out)           :: reflectance_VL_SURF(nobs, nch) ! TOA reflectance from VLIDORT using surface module
+    integer,          intent(out)           :: rc                             ! return code
+
+    real*8,           intent(out)           :: ROT(km,nobs,nch)               ! rayleigh optical thickness
+    real*8,           intent(out)           :: BR(nobs,nch)                   ! bidirectional reflectance 
+    real*8,           intent(out)           :: Q(nobs, nch)                   ! Stokes parameter Q
+    real*8,           intent(out)           :: U(nobs, nch)                   ! Stokes parameter U   
+    real*8,           intent(out)           :: BR_Q(nobs, nch)                ! Stokes parameter Q
+    real*8,           intent(out)           :: BR_U(nobs, nch)                ! Stokes parameter U   
+
+
+    call VLIDORT_Vector_Lambert_BPDF (km, nch, nobs, channels, nMom, &
+                                   nPol, tau, ssa, pmom, pe, he, te, &
+                                   albedo, BPDFparam, &
+                                   solar_zenith, &
+                                   relat_azymuth, &
+                                   sensor_zenith, &
+                                   MISSING,verbose, &
+                                   radiance_VL_SURF, &
+                                   reflectance_VL_SURF, &
+                                   BR, ROT, Q, U, BR_Q, BR_U, rc )  
+
+
+end subroutine VECTOR_LAMBERT_BPDF
+
+
 subroutine SCALAR_LAMBERT(km, nch, nobs, channels, nMom, nPol, &
                      tau, ssa, g, pmom, pe, he, te, albedo, &
                      solar_zenith, relat_azymuth, sensor_zenith, &
