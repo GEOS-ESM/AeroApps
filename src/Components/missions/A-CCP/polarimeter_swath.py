de8c963db252c0779d715e8a744009cb20df619e
Patricia Castellanos <patricia.castellanos@nasa.gov>
2019-12-23 14:45:11 -0500

    PC - work in progress

--- src/Components/missions/A-CCP/polarimeter_swath.py
+++ src/Components/missions/A-CCP/polarimeter_swath.py
@@ -13,14 +13,102 @@ from   datetime        import datetime, timedelta
 from   dateutil.parser import parse         as isoparser
 from   MAPL            import Config
 import numpy  as np
+from netCDF4 import Dataset
+import LidarAngles_   
+
+# Generic Lists of Varnames and Units
+SDS_AER    = ['LONGITUDE','LATITUDE','isotime']
 
 
+ncALIAS = {'LONGITUDE': 'trjLon',
+           'LATITUDE': 'trjLat'}
 
 
+class SWATH(object):
+    """
+    get polarimeter swath
+    """
+    def __init__(self,inFile,outFile,hgtss):
+        self.inFile = inFile
+        self.outFile = outFile
+        self.hgtss     = hgtss
+        self.SDS_AER    = SDS_AER
+
+        # initialize empty lists
+        for sds in self.SDS_AER:
+            self.__dict__[sds] = []
+
+        # Read in model data
+        self.readSampledGEOS()
+
+        # Make lists into arrays
+        for sds in self.SDS_AER:
+            self.__dict__[sds] = np.concatenate(self.__dict__[sds])
+
+        # convert isotime to datetime
+        self.tyme = []
+        for isotime in self.isotime:
+            self.tyme.append(isoparser(''.join(isotime)))
+
+        self.tyme = np.array(self.tyme)
+
+        # Calculate Nadir Scene Geometry
+        self.hgtss = hgtss
+        self.nadirAngles()
+    # --
+    def nadirAngles(self):
+        SZA   = []
+        SAA   = []
+        VAA   = []
+
+        for i,tyme in enumerate(self.tyme[:-2]):
+            CLAT = self.LATITUDE[i]
+            CLON = self.LONGITUDE[i]
+            
+            SLAT = self.LATITUDE[i]
+            SLON = self.LONGITUDE[i]
+            sat_angles = LidarAngles_.satangles(tyme.year,tyme.month,tyme.day,
+                                                tyme.hour,tyme.minute,tyme.second,
+                                                CLAT,CLON,
+                                                SLAT,SLON,
+                                                0.0,
+                                                self.hgtss)
+            SZA.append(sat_angles[3][0])
+            SAA.append(sat_angles[2][0])
 
 
+            CLAT = self.LATITUDE[i+1]
+            CLON = self.LONGITUDE[i+1]
+            sat_angles = LidarAngles_.satangles(tyme.year,tyme.month,tyme.day,
+                                                tyme.hour,tyme.minute,tyme.second,
+                                                CLAT,CLON,
+                                                SLAT,SLON,
+                                                0.0,
+                                                self.hgtss)
+
+            VAA.append(sat_angles[0][0])
 
 
+        self.SZA = np.array(SZA)
+        self.SAA = np.array(SAA)
+        self.VAA = np.array(VAA)     
+
+    #---
+    def readSampledGEOS(self):
+        """
+        Read in model sampled track
+        """
+        col = 'aer_Nv'
+        if self.verbose: 
+            print 'opening file',self.inFile.replace('%col',col)
+        nc       = Dataset(self.inFile.replace('%col',col))
+
+        for sds in self.SDS_AER:
+            sds_ = sds
+            if sds in ncALIAS:
+                sds_ = ncALIAS[sds]
+            var = nc.variables[sds_][:]
+            self.__dict__[sds].append(var)
 
 
 
@@ -77,7 +165,7 @@ if __name__ == "__main__":
         day   = str(date.day).zfill(2)
         hour  = str(date.hour).zfill(2)    
 
-        inFile     = inTemplate.replace('%col','aer_Nv').replace('%year',year).replace('%month',month).replace('%day',day).replace('%nymd',nymd).replace('%hour',hour).replace('%instname',instname).replace('%INSTNAME',INSTNAME)
+        inFile     = inTemplate.replace('%year',year).replace('%month',month).replace('%day',day).replace('%nymd',nymd).replace('%hour',hour).replace('%instname',instname).replace('%INSTNAME',INSTNAME)
         outFile    = inTemplate.replace('%col',polarimeter_name).replace('%year',year).replace('%month',month).replace('%day',day).replace('%nymd',nymd).replace('%hour',hour).replace('%instname',instname).replace('%INSTNAME',INSTNAME)
 
 
@@ -90,4 +178,4 @@ if __name__ == "__main__":
         print '>>>verbose:   ',args.verbose
         print '++++End of arguments+++'
         if not args.dryrun:
-            swath = SWATH(inFile,
+            swath = SWATH(inFile,outFile,HGT)
