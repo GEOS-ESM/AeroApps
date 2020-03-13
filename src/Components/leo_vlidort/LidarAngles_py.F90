bdd877fa54bb651a0344dfc05b5666ce60dff40a
Patricia Castellanos <patricia.castellanos@nasa.gov>
2020-03-13 15:03:56 -0400

    PC - python interface for both from space and from traget view angles

--- src/Components/leo_vlidort/LidarAngles_py.F90
+++ src/Components/leo_vlidort/LidarAngles_py.F90
@@ -720,15 +720,14 @@ end function view_angles_from_space
 
 !;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ! NAME
-!    satellite_angles
+!    satellite_angles_from_space
 !
 ! PURPOSE
-!    For a satellite, at a given target location,
+!    For a satellite and a given target location,
 !    calculate spacecraft Azimuth Viewing Angle VAA
-!    and spacecraft Zenith Viewing Angle VZA;
+!    and spacecraft Viewing Angle VA;
 !    also given UTC time calculate
 !    Solar Zenith Angle (SZA) and Solar Azimuth Angle(SAA).
-!    (User may then calculate Relative Azimuth Angle RAA = VAA-SAA)
 !
 ! INPUTS
 !    Yr,Mo,Day,Hr,Min,Sec = UTC Time
@@ -806,6 +805,95 @@ satellite_angles(4) = sza
 return
 end function satellite_angles_from_space
 
+!;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+! NAME
+!    satellite_angles
+!
+! PURPOSE
+!    For a satellite, at a given target location,
+!    calculate spacecraft Azimuth Viewing Angle VAA
+!    and spacecraft Zenith Viewing Angle VZA;
+!    also given UTC time calculate
+!    Solar Zenith Angle (SZA) and Solar Azimuth Angle(SAA).
+!    (User may then calculate Relative Azimuth Angle RAA = VAA-SAA)
+!
+! INPUTS
+!    Yr,Mo,Day,Hr,Min,Sec = UTC Time
+!    tz    = local time zone (0.d0 = UTC, -8.d0 = PST)
+!    Latp  = latitude of target in degrees (-90 to 90)
+!    Lonp  = longitude of target in degrees (-180 to 180)
+!    Latss = latitude of satellite in degrees (-90 to 90)
+!    Lonss = longitude of satellite in degrees (-180 to 180)
+!    hgtss = altitude of satellite in km
+!
+! OUTPUTS
+!   satellite_angles    = returned array of length 4
+!   satellite_angles(1) = Spacecraft Viewing Azimuth Angle in deg (0-360)
+!   satellite_angles(2) = Spacecraft Viewing Zenith Angle in deg (0-90)
+!   satellite_angles(3) = Solar Azimuth Angle in degrees (0-360)
+!   satellite_angles(4) = Solar Zenith Angles in degrees (0-180, >90 is below horizon)
+!
+! REQUIRED
+!   solar_angles.f90
+!   view_angles_from_target.f90
+!   julday.f90
+!
+! HISTORY
+!   written 29 April 2010 by Joyce Wolf for Annmarie Eldering & S. Kulawik
+!   translated into Fortran90 24 Feb 2012 by Vijay Natraj
+!;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+FUNCTION satellite_angles(yr,mo,day,hr,min,sec,tz,latp,lonp,latss,lonss,hgtss)
+
+implicit none
+
+!  Inputs
+
+integer(kind=4), intent(in) :: yr
+integer(kind=4), intent(in) :: mo
+integer(kind=4), intent(in) :: day
+integer(kind=4), intent(in) :: hr
+integer(kind=4), intent(in) :: min
+real(kind=8),    intent(in) :: sec
+real(kind=8),    intent(in) :: tz
+real(kind=8),    intent(in) :: latp  ! latitude of target in degrees (-90 to 90)
+real(kind=8),    intent(in) :: lonp  ! longitude of target in degrees (-180 to 180)
+real(kind=8),    intent(in) :: latss ! latitude of satellite in degrees (-90 to 90)
+real(kind=8),    intent(in) :: lonss ! longitude of satellite in degrees (-180 to 180)
+real(kind=8),    intent(in) :: hgtss ! altitude of satellite in km
+
+!  Outputs
+
+real(kind=8) :: satellite_angles(4)
+                             ! 1: Spacecraft Viewing Azimuth Angle in deg (0-360)
+                             ! 2: Spacecraft Viewing Zenith Angle in deg (0-90)
+                             ! 3: Solar Azimuth Angle in degrees (0-360)
+                             ! 4: Solar Zenith Angles in degrees (0-180), 
+                             !    >90 is below horizon)
+
+!  Local variables
+
+real(kind=8)                :: vangles(2), phiv, thetav
+real(kind=8)                :: azel(2), saa, sza
+
+vangles = view_angles_from_target(latp,lonp,latss,lonss,hgtss)
+phiv    = vangles(1)
+thetav  = 90.0 - vangles(2)
+
+azel = solar_angles(yr,mo,day,hr,min,sec,tz,latp,lonp)
+saa = azel(1)
+sza = 90.d0 - azel(2)
+
+satellite_angles(1) = phiv
+satellite_angles(2) = thetav
+satellite_angles(3) = saa
+satellite_angles(4) = sza
+
+return
+end function satellite_angles
+
+
+
 end Module LidarAngles
 
 ! -------------------------- Python Interface -------------------
@@ -862,6 +950,63 @@ real(kind=8), intent(out) :: solar_zenith(n)
   end do
 
 end subroutine satAngles
+
+! -------------------------- Python Interface -------------------
+
+subroutine satAngles_fromSpace(n,yr,mo,day,hr,min,sec,latp,lonp,latss,lonss,tz,hgtss, &
+                     view_azimuth, view_angle,  &
+                     solar_azimuth, solar_zenith )
+
+use LidarAngles
+
+implicit none
+
+!  Inputs
+
+integer(kind=4), intent(in) :: n        ! number of points
+integer(kind=4), intent(in) :: yr(n)    ! UTC year
+integer(kind=4), intent(in) :: mo(n)    ! UTC month
+integer(kind=4), intent(in) :: day(n)   ! UTC day
+integer(kind=4), intent(in) :: hr(n)    ! UTC hour
+integer(kind=4), intent(in) :: min(n)   ! UTC min
+real(kind=8),    intent(in) :: sec(n)   ! UTC second
+real(kind=8),    intent(in) :: lonp(n)  ! longitude of target in degrees (-180 to 180)
+real(kind=8),    intent(in) :: latp(n)  ! latitude of target in degrees (-90 to 90)
+real(kind=8),    intent(in) :: lonss(n) ! longitude of satellite in degrees (-180 to 180)
+real(kind=8),    intent(in) :: latss(n) ! latitude of satellite in degrees (-90 to 90)
+real(kind=8),    intent(in) :: tz       ! local time zone (0.d0 = UTC, -8.d0 = PST)
+real(kind=8),    intent(in) :: hgtss    ! altitude of satellite in km
+
+
+
+!  Outputs
+
+real(kind=8), intent(out) :: view_azimuth(n)
+real(kind=8), intent(out) :: view_angle(n)
+real(kind=8), intent(out) :: solar_azimuth(n)
+real(kind=8), intent(out) :: solar_zenith(n)
+
+!                ---
+
+  integer(kind=4) :: i
+
+  real(kind=8) :: sat_angles(4)
+
+  do i = 1, n
+
+     sat_angles = satellite_angles_from_space(yr(i),mo(i),day(i),hr(i),min(i),sec(i),tz, &
+                                   latp(i),lonp(i),latss(i),lonss(i),hgtss)
+
+     view_azimuth(i)  = sat_angles(1)
+     view_angle(i)   = sat_angles(2)
+     solar_azimuth(i) = sat_angles(3)
+     solar_zenith(i)  = sat_angles(4)
+
+  end do
+
+end subroutine satAngles_fromSpace
+
+
 
 subroutine solarAngles(n,yr,mo,day,hr,min,sec,latp,lonp,tz, &
                      solar_azimuth, solar_zenith )
