e13b7812d7633bdcf3b9e1e3c50353b8718f2e4a
Patricia Castellanos <patricia.castellanos@nasa.gov>
2023-03-20 16:52:46 -0400

    Revert "PC - fix integer division and convert f2py returned strings from byte to unicode"
    This reverts commit b12cdcdfad9694852d647b637e5aaf41fb0cf5c9.
    
    Should not have merge develop into this branch.

--- src/Shared/GMAO_Shared/GMAO_gfio/gfio.py
+++ src/Shared/GMAO_Shared/GMAO_gfio/gfio.py
@@ -75,10 +75,6 @@ class GFIO(object):
         self.valid_range, self.packing_range,rc                             \
             = gfioinquire(self.fid,self.im,self.jm,km_,self.lm,self.nvars)
 
-#       Convert byte string to unicode
-        strlist = ['title','source','contact','levunits','vname','vtitle','vunits']
-        for strname in strlist:
-            self.__dict__[strname] = self.__dict__[strname].decode()
         if rc==3:
             raise GFIOError("cannot get COARDS metadata for GFIO file " \
                            + filename + ", rc = " + str(rc))
@@ -935,14 +931,14 @@ def _strTemplate(templ,expid=None,nymd=None,nhms=None,
 
     if nymd is not None:
         nymd = int(nymd)
-        yy = nymd//10000
-        mm = (nymd - yy*10000)//100
+        yy = nymd/10000
+        mm = (nymd - yy*10000)/100
         dd = nymd - (10000*yy + 100*mm )
 
     if nhms is not None:
         nhms = int(nhms)
-        h = nhms//10000
-        m = (nhms - h * 10000)//100
+        h = nhms/10000
+        m = (nhms - h * 10000)/100
         s = nhms - (10000*h + 100*m)
 
     if expid is not None: 
