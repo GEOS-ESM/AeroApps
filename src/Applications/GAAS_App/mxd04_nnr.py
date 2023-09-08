fix MODIS ae calculation. also do extra contamination filtering on AE if needed

src/Applications/GAAS_App/mxd04_nnr.py
@@ -486,7 +486,6 @@ class MxD04_NNR(MxD04_L2):
             for i,targetName in enumerate(self.net.TargetNames):
                     if 'AEfitm' in targetName:
                         AEfitm = targets[:,i]
-                        self.ae_ = AEfitm
                     if 'AEfitb' in targetName:
                         AEfitb = targets[:,i]
             nobs = targets.shape[0]
@@ -498,11 +497,6 @@ class MxD04_NNR(MxD04_L2):
 
             targets = targets_
             self.net.TargetNames = targetName
-
-            # calculate MODIS standard retrieval AE
-            # ------------------------------------
-            fit = np.polyfit(np.log(self.channels),-1.*np.log(self.aod+0.01),1)
-            self.ae = fit[0,:]
 
 
         if doAE:
@@ -555,6 +549,21 @@ class MxD04_NNR(MxD04_L2):
 
             self.__dict__[name][self.iGood,k] = result
 
+        if doAEfit:
+            # Save predicted angstrom exponent
+            self.ae_ = MISSING*ones(self.nobs) 
+            self.ae_[self.iGood] = AEfitm
+
+            # calculate MODIS standard retrieval AE
+            # ------------------------------------
+            I = []
+            for ch in self.channels_:
+                i = list(self.channels).index(ch)
+                I = I + [i,]           
+            aodT = self.aod[:,I].T
+            fit = np.polyfit(np.log(self.channels_),-1.*np.log(aodT[:,self.iGood]+0.01),1)
+            self.ae = MISSING*ones(self.nobs)
+            self.ae[self.iGood] = fit[0,:]            
 
         # Do extra cloud filtering if required
         if self.cloudFree is not None:                 
@@ -577,10 +586,11 @@ class MxD04_NNR(MxD04_L2):
                 k = list(self.channels).index(ch) # index of channel
                 self.__dict__[name][self.iGood,k][contaminated] = MISSING
 
+            if doAEfit:
+                self.ae[self.iGood][contaminated] = MISSING
+                self.ae_[self.iGood][contaminated] = MISSING
+
             self.iGood[self.iGood][contaminated] = False
-
-
-
 
 
 #---