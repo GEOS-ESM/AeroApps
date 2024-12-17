4d7f077cc49e45d88ec992f1ade1ed70e71d512b
Patricia Castellanos <patricia.castellanos@nasa.gov>
2024-12-17 17:09:58 -0500

    initial commit of code to create L1c files to aos-sky

---
+++ src/Components/missions/AOS-sky/run_polarimeter_swath.j
@@ -0,0 +1,49 @@
+#!/bin/csh -f
+
+#######################################################################
+#                     Batch Parameters for Run Job
+#######################################################################
+#SBATCH --time=12:00:00
+#SBATCH --ntasks=8 --cpus-per-task=1 --ntasks-per-node=8
+#SBATCH --job-name=swath
+#SBATCH -A s2190
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=patricia.castellanos@nasa.gov
+#SBATCH --output=slurm_%j.out
+#SBATCH --error=slurm_%j.err
+#######################################################################
+#                  System Environment Variables
+#######################################################################
+
+umask 022
+
+limit stacksize unlimited
+
+#######################################################################
+#           Architecture Specific Environment Variables
+#######################################################################
+setenv G5DIR /discover/nobackup/pcastell/workspace/GAAS/src
+setenv BIN $PWD 
+setenv TMPDIR $LOCAL_TMPDIR 
+
+mkdir -p $LOCAL_TMPDIR/tle
+cp -r /discover/nobackup/pcastell/workspace/GAAS/src/Components/missions/A-CCP/tle/ $LOCAL_TMPDIR
+ln -s $LOCAL_TMPDIR/tle ./tle
+
+source $HOME/.cshrc
+cd $G5DIR
+source g5_modules
+
+#######################################################################
+#   Move to Run Directory
+#######################################################################
+cd $BIN
+
+##################################################################
+######
+######         Do Sampling
+######
+##################################################################
+python -u run_accp_polarimeter_swath.py -v --DT_hours 1 2006-01-01T00:00 2006-01-02T00:00 lidar_files.pcf gpm.pcf polar07.pcf  > slurm_${SLURM_JOBID}_py.out &
+rm -rf $LOCAL_TMPDIR/*
+rm tle
---
+++ src/Components/missions/AOS-sky/run_polarimeter_swath.py
@@ -0,0 +1,315 @@
+#!/usr/bin/env python3
+
+"""
+    Wrapper to submit jobs to sbatch
+"""
+
+import os
+import subprocess
+import shutil
+from   datetime        import datetime, timedelta
+from   dateutil.parser import parse         as isoparser
+import argparse
+import numpy as np
+import time
+
+class JOBS(object):
+    def handle_jobs(self):
+        jobsmax = 25
+        # Figure out how many jobs you need to submit
+        numjobs  = len(self.dirstring)   
+        
+        devnull = open(os.devnull, 'w')
+        if numjobs <= jobsmax:   
+            countRun     = numjobs 
+            node_tally   = numjobs
+        else:
+            countRun   = jobsmax
+            node_tally = jobsmax
+
+        workingJobs = np.arange(countRun)
+        
+
+        # Submit first JOBSMAX jobs
+        jobid = np.empty(0)
+        for i in workingJobs:
+            s = self.dirstring[i]
+            os.chdir(s)
+            result = subprocess.check_output(['sbatch',self.slurm])
+            jobid = np.append(jobid,result.split()[-1])
+        os.chdir(self.cwd)
+
+        # launch subprocess that will monitor queue
+
+        # Monitor jobs 1-by-1 
+        # Add a new job when one finishes 
+        # Until they are all done
+        stat = 0
+        countDone = 0
+        while (stat == 0):
+            finishedJobs = np.empty(0,dtype='int')
+            for ii,i in enumerate(workingJobs):
+                s = jobid[i]
+                s = s.strip('\n')
+                finished = False
+
+                # Check to see if this job is finished
+                result = subprocess.call(['qstat',s], stdout=devnull)
+                if (result != 0):
+                    finished = True   
+
+                # if the job is finished add to finished jobs list
+                # and clean up workspace
+                if finished:
+                    #print 'Job finished, cleaning up', s, i 
+                    finishedJobs = np.append(finishedJobs,ii)
+                    countDone += 1
+                    errcheck = self.check_for_errors(i,s)               
+                    if (errcheck is False):
+                        self.errTally[i] = False
+
+                        #clean up workspaces
+                        self.destroy_workspace(i,s)
+
+                    else:
+                        print('Jobid ',s,' in ',self.dirstring[i],' exited with errors')
+
+            # finished checking up on all the jobs
+            # Remove finished jobs from the currently working list
+            if len(finishedJobs) != 0:
+                print('deleting finishedJobs',finishedJobs,jobid[workingJobs[finishedJobs]])
+                node_tally  = node_tally - len(finishedJobs)
+
+                workingJobs = np.delete(workingJobs,finishedJobs)
+
+            # Add more jobs if needed
+            # reinitialize stat variable
+            if (numjobs > countRun) and (node_tally < jobsmax):
+                #print 'adding new jobs'
+                newRun     = jobsmax - node_tally
+                node_tally = jobsmax
+
+                if (newRun + countRun) > numjobs:
+                    newRun = numjobs - countRun
+                    node_tally = node_tally + newRun
+                
+
+                newjobs  = countRun + np.arange(newRun)
+                workingJobs = np.append(workingJobs, newjobs)
+                for i in newjobs:
+                    s = self.dirstring[i]
+                    os.chdir(s)
+                    result = subprocess.check_output(['sbatch',self.slurm])
+                    jobid = np.append(jobid,result.split()[-1])
+
+                os.chdir(self.cwd)
+                countRun = countRun + newRun
+
+
+            # check if all the jobs are finished
+            if countDone == numjobs:
+                stat = 1
+            else:
+                print('Waiting 30 minutes')
+                time.sleep(60*30)
+            
+
+        # Exited while loop
+        print('All jobs done')
+
+        devnull.close()
+
+
+    def check_for_errors(self,i,jobid):
+        os.chdir(self.dirstring[i])  
+
+        error = False 
+        errfile = 'slurm_' +jobid + '.err'
+        statinfo = os.stat(errfile)
+        if (statinfo.st_size != 0):
+            error = True
+
+        os.chdir(self.cwd)
+        return error
+
+class WORKSPACE(JOBS):
+    """ Create slurm scripts for running mp_accp_polar_vlidort.py """
+    def __init__(self,args):
+
+        self.Date      = isoparser(args.iso_t1)
+        self.enddate   = isoparser(args.iso_t2)
+        self.Dt        = timedelta(hours=args.DT_hours)
+        self.dt        = timedelta(days=args.dt_days)
+        
+        self.track_pcf   = args.track_pcf
+        self.orbit_pcf   = args.orbit_pcf
+        self.inst_pcf    = args.inst_pcf
+        self.DT_hours    = args.DT_hours
+        self.dryrun      = args.dryrun
+
+        self.slurm       = args.slurm
+        self.tmp         = args.tmp
+        self.profile     = args.profile
+        self.single      = args.single
+
+        if not os.path.exists(args.tmp):
+            os.makedirs(args.tmp)
+
+        self.cwd         = os.getcwd()
+
+        # create working directories
+        self.create_workdir()
+        self.errTally    = np.ones(len(self.dirstring)).astype(bool)
+
+
+    def create_workdir(self):
+        sdate = self.Date
+        self.dirstring = []
+        while sdate < self.enddate:
+            # create directory
+
+            workpath = '{}/{}'.format(self.tmp,sdate.isoformat())
+            if os.path.exists(workpath):
+                shutil.rmtree(workpath)
+
+            os.makedirs(workpath)
+
+            # copy over slurm scipt
+            outfile = '{}/{}'.format(workpath,self.slurm)
+            shutil.copyfile(self.slurm,outfile)
+
+            # copy over pcf files                
+            outfile = '{}/{}'.format(workpath,self.track_pcf)
+            shutil.copyfile(self.track_pcf,outfile)
+
+            outfile = '{}/{}'.format(workpath,self.orbit_pcf)
+            shutil.copyfile(self.orbit_pcf,outfile)
+
+            outfile = '{}/{}'.format(workpath,self.inst_pcf)
+            shutil.copyfile(self.inst_pcf,outfile)
+
+            #link over needed python scripts
+            source = ['polarimeter_swath.py'] 
+            for src in source:
+                os.symlink('{}/{}'.format(self.cwd,src),'{}/{}'.format(workpath,src))
+
+            # edit slurm
+            self.edit_slurm(sdate,workpath)
+
+            self.dirstring.append(workpath)
+            
+            sdate += self.dt
+
+    def edit_slurm(self,sdate,workpath):
+        edate = sdate + self.dt
+        if edate > self.enddate:
+            edate = self.enddate
+        outpath = '{}/{}'.format(workpath,self.slurm)
+
+        # read file first
+        f = open(outpath)
+
+        text = []
+        for l in f:
+            text.append(l)
+
+        # replace one line
+        iso1 = sdate.isoformat()
+        iso2 = edate.isoformat()
+        Options = ' -v' +\
+                  ' --DT_hours {}'.format(self.DT_hours) 
+
+        if self.dryrun:
+            Options += ' -r'
+
+        if self.single:
+            Options += ' --single'
+
+        newline = 'python -u ./polarimeter_swath_single.py {} {} {} {} {} {}  >'.format(Options,iso1,iso2,self.track_pcf,self.orbit_pcf,self.inst_pcf) + ' slurm_${SLURM_JOBID}_py.out\n'
+        text[-3] = newline
+        f.close()
+
+        #  write out
+        f = open(outpath,'w')
+        for l in text:
+            f.write(l)
+        f.close()             
+
+    def destroy_workspace(self,i,jobid):
+        os.chdir(self.dirstring[i])
+
+        if self.profile is False:
+            
+            errfile = 'slurm_' +jobid + '.err'
+            os.remove(errfile)       
+            outfile = 'slurm_' +jobid + '.out' 
+            os.remove(outfile)        
+
+            outfile = 'slurm_' +jobid + '_py.out'
+            os.remove(outfile)     
+
+            os.remove(self.slurm)
+            os.remove(self.track_pcf)
+            os.remove(self.orbit_pcf)
+            os.remove(self.inst_pcf)
+
+        # remove symlinks
+        source = ['polarimeter_swath.py'] 
+        for src in source:
+            os.remove(src)
+
+        os.chdir(self.cwd)
+        if self.profile is False:
+            os.rmdir(self.dirstring[i])
+
+
+if __name__ == '__main__':
+    
+    #Defaults
+    DT_hours = 1
+    dt_days  = 10
+    slurm    = 'run_polarimeter_swath.j'
+    tmp      = '/discover/nobackup/projects/gmao/osse2/pub/c1440_NR/OBS/A-CCP/workdir/swath'
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("iso_t1",help='starting iso time')
+    parser.add_argument("iso_t2",help='ending iso time')
+    parser.add_argument("track_pcf",
+                        help="prep config file with track input file names")
+
+    parser.add_argument("orbit_pcf",
+                        help="prep config file with orbit variables")
+
+    parser.add_argument("inst_pcf",
+                        help="prep config file with instrument variables")
+
+    parser.add_argument('-d',"--dt_days", default=dt_days, type=int,
+                        help="Timestep in days for each job (default=%i)"%dt_days)
+
+    parser.add_argument('-D',"--DT_hours", default=DT_hours, type=int,
+                        help="Timestep in hours for each file (default=%i)"%DT_hours)
+
+    parser.add_argument('-s',"--slurm",default=slurm,
+                        help="slurm script template (default=%s)"%slurm)           
+
+    parser.add_argument('-t','--tmp',default=tmp,
+                        help="temp directory (default=%s)"%tmp)
+
+    parser.add_argument("-r", "--dryrun",action="store_true",
+                        help="do a dry run (default=False).") 
+
+    parser.add_argument("-p", "--profile",action="store_true",
+                        help="Don't cleanup slurm files (default=False).")   
+
+    parser.add_argument("--single",action="store_true",
+                        help="Only do 1 orbit per day (default=False).")
+
+    args = parser.parse_args()
+    if args.single:
+        args.DT_hours = 24
+
+    workspace = WORKSPACE(args)
+
+    if not args.dryrun:
+        workspace.handle_jobs()
+
