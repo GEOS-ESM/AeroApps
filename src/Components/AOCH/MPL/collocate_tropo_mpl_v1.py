48f804905095601cc98e55e80f8c8148a551d7da
Santiago Gassó <sgasso@umd.edu>
2025-03-05 16:47:30 -0500

    Modified collocate_tropo_mpl_v1.py

--- src/Components/AOCH/MPL/collocate_tropo_mpl_v1.py
+++ src/Components/AOCH/MPL/collocate_tropo_mpl_v1.py
@@ -39,9 +39,11 @@ from datetime import datetime, timedelta
 from pyobs.tropomi_l2_reader import TROPOAER_L2
 
 
-import f_collocate_1   as fc #needed for finding pixels that contain site 
-import f_find_orbits_1 as fo # needed for finding orbits that contain sites
+# import f_collocate_1   as fc #needed for finding pixels that contain site 
+# import f_find_orbits_1 as fo # needed for finding orbits that contain sites
  
+import test_f_collocate_2   as fc #needed for finding pixels that contain site 
+import test_f_find_orbits_2 as fo # needed for finding orbits that contain sites
 
 
 if __name__ == "__main__":
@@ -52,7 +54,7 @@ if __name__ == "__main__":
     verb = True
     #yyyy0,mm0,dd0=[2023,1,1]; yyyy1,mm1,dd1=[2023,12,31]
     yyyy0,mm0,dd0=[2023,8,10]; yyyy1,mm1,dd1=[2023,8,10]
-    user_string = "TEST" # additional string to add to output file SiteName_yyyy_<user_string>.txt
+    user_string = "TEST_new" # additional string to add to output file SiteName_yyyy_<user_string>.txt
     #julian=359 
 
     #### End user input section 
@@ -100,7 +102,7 @@ if __name__ == "__main__":
          
         #### Now for orbits available for this day, find which ones contain a site 
         matching_orbits_list,current_pth =\
-                fo.get_orbit_site_matches(mpl_sites_list,yyyy, mm, dd,verb)
+                fo.get_orbit_site_matches2(mpl_sites_list,yyyy, mm, dd,verb)
         # orbit_matches_sit = fo.get_orbit_site_matches(mpl_sites_list,yyyy=2023, julian=359,verb=True) 
         if  len(matching_orbits_list)==0 : 
             print('No orbits found that contain the sites. Check ', current_pth)
@@ -123,9 +125,12 @@ if __name__ == "__main__":
         #### next value for iteration
         current_date += timedelta(days=1)
     
+    #### Done with loop over each day
     end_time = datetime.now()
     time_diff = (end_time - start_time).total_seconds()
     print(f"\nLoop Execution time: {time_diff:.2f} seconds")
+    
+    
     ### Save one file per site
     ### Inputs: array_to_save, year_string,user_string
     fc.create_output_files(combined_results, "2023", user_string)
