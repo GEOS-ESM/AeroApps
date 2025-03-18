0b92bdf512e23bf436a3f60d392f3a6134f84850
Patricia Castellanos <patricia.castellanos@nasa.gov>
2025-03-18 11:22:18 -0400

    no longer need copyvar

--- src/Components/missions/SBG/sbg_vlidort.py
+++ src/Components/missions/SBG/sbg_vlidort.py
@@ -28,7 +28,6 @@ from pyobs.constants import MAPL_KAPPA as KAPPA
 
 
 from py_leo_vlidort.vlidort import VLIDORT, get_chd, WrapperFuncs, MODIS_BRDF_run
-from py_leo_vlidort.copyvar  import _copyVar
 from multiprocessing import Pool
 
 # Generic Lists of Varnames and Units
