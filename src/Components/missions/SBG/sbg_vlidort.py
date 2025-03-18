eba1d32d05ed88c1462738a59b88607f6cbe6f42
Patricia Castellanos <patricia.castellanos@nasa.gov>
2025-03-18 11:25:27 -0400

    no longer using mietables

--- src/Components/missions/SBG/sbg_vlidort.py
+++ src/Components/missions/SBG/sbg_vlidort.py
@@ -19,7 +19,6 @@ from   dateutil.parser import parse         as isoparser
 from   MAPL.config     import Config
 import numpy   as np
 import xarray  as xr
-from pyobs import mietable as mt
 import yaml
 from pyobs.constants import MAPL_GRAV as GRAV
 from pyobs.constants import MAPL_RDRY as RGAS
