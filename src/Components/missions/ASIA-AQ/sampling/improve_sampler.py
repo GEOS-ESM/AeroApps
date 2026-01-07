9f78248572590ab726a48cec8be3fdc37ccdb4f0
Patricia Castellanos <patricia.castellanos@nasa.gov>
2026-01-07 14:21:04 -0500

    update improve_sampler to use dask

--- src/Components/missions/ASIA-AQ/sampling/improve_sampler.py
+++ src/Components/missions/ASIA-AQ/sampling/improve_sampler.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
 """
     Sampler for IMPROVE surface PM2.5 obs for a modeling experiment
+    This uses a parquet reference store to read the dataset
+    Run ctl2kerchunk_run.j followed by kerchunk2parquet_run.j to create it
 """
 import os, sys
 import argparse
 import yaml
 from glob import glob
-from datetime import datetime
+from datetime import datetime, timedelta
 from pyobs.sampler import STATION
 from pyobs.improve import SITE_MAP
 from pyobs.xrctl import parse_ctl
 from dask.distributed import performance_report
 from dask.distributed import Client, LocalCluster
+import dask.dataframe as dd
 if __name__ == '__main__':
 
 
@@ -23,79 +26,49 @@ if __name__ == '__main__':
 
     config = yaml.safe_load(open(args.config))
 
-    # create output directory
-    if not os.path.exists(config['sampled_outdir']):
-        os.makedirs(config['sampled_outdir'])
-
-    # get IMPROVE site locations 
+    # get IMPROVE site locations
     site_path = config['improve_site_map']
     sites = SITE_MAP(site_path)
 
-    # get years to sample
-    years = config['improve_years']
+    # create output directory
+    outdir = config['sampled_outdir'] + '/improve'
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
 
-    kwargs = dict(n_workers=120, threads_per_worker=1, memory_limit='4GB')
-    with LocalCluster(**kwargs) as cluster, Client(cluster) as cluster:
-        for year in years:
-            print(f"+++++++ IMPROVE Sampling on {site_path}")
-            time_range = [datetime(year,1,1),datetime(year,12,31)]
-            station,lon,lat = sites.df['SiteCode'],sites.df['Longitude'],sites.df['Latitude']
+    # get reference parquet location
+    refdir = config['combined_references']
 
-            # Sample Aerosol Collection
-            # --------------------------------------
-            ctl = config['model_aer_ctl'] 
-            chunks = {'time':1, 'lev':-1, 'lat':-1, 'lon': -1}
-            # check that all files exist
-            paths = parse_ctl(ctl,time_range)
-            exists = True
-            for p in paths:
-                if not os.path.exists(p): exists = False
-            
-            if exists:
-                stn = STATION(station,lon,lat,ctl,time_range=time_range,verbose=True,chunks=chunks,engine='h5netcdf')
-                
+    n_workers = 120
+    kwargs = dict(n_workers=n_workers, threads_per_worker=1, memory_limit='4GB')
+    with LocalCluster(**kwargs) as cluster, Client(cluster) as client:
+        print(f"+++++++ IMPROVE Sampling on {site_path}")
+        station,lon,lat = sites.df['SiteCode'].values,sites.df['Longitude'].values,sites.df['Latitude'].values
+
+        # Sample Aerosol Collection
+        # --------------------------------------
+        ctls = [config['model_aer_ctl']] + config['model_other_ctl']
+        chunks = {'time':1, 'lev':-1, 'lat':-1, 'lon': -1}
+        for ctl in ctls:
+            if ctl:
+                # get reference parquet
+                outparq = refdir + f'/reference_store_{ctl}.parq'
+                     
+                print(f" Sampling on reference {outparq}")
+                stn = STATION(station,lon,lat,outparq,verbose=True,chunks=chunks,engine='h5netcdf')
+                    
                 stn_ds = stn.sample()
-                if args.profile:
-                    with performance_report(filename="dask-report.html"):
-                        stn_ds = stn_ds.compute()
-                else:
-                    stn_ds = stn_ds.compute()
 
                 # write out the native sampled model fields
-                outFile = config['sampled_outdir'] + '/improve.'  + ctl + '.nc4'
-                stn_ds.to_netcdf(outFile,engine='netcdf4')
-
-                # If other model collections are provided
-                # sample those and write out
-                # -----------------------------
-                for ctl in config['model_other_ctl']:
-                    if ctl is not None:
-                        try:
-                            # check that all files exist
-                            paths = parse_ctl(ctl,time_range)
-                            exists = True
-                            for p in paths:
-                                if not os.path.exists(p): exists = False
-
-                            if exists:
-                                stn = STATION(station,lon,lat,ctl,time_range=time_range,verbose=True,chunks=chunks,engine='h5netcdf')
-                            else:
-                                print(f"Model input files missing for {year}")
-                                print("Skipping...")
-                                continue
-                        except:
-                            print(f"Error reading model for {year}")
-                            print("Skipping....")
-                            continue
-
-                        stn_ds = stn.sample()
-                        stn_ds = stn_ds.compute()
-
-                        # write out the native sampled model fields
-                        outFile = config['sampled_outdir'] + '/improve.' + ctl + '.nc4'
-                        stn_ds.to_netcdf(outFile,engine='netcdf4')
+                outFile = f'{outdir}/improve.{ctl}.nc4'
+                if args.profile:
+                    with performance_report(filename="dask-report.html"):
+                        stn_ds.to_netcdf(outFile,engine='h5netcdf',compute=True)
+                else:
+                    stn_ds.to_netcdf(outFile,engine='h5netcdf',compute=True)
+#                    stn_ds = stn_ds.compute()
 
             else:
-                print(f"Model input files missing for {year}")
-                print("Skipping...")
-                continue
+                print(f"Model input files missing for {ctl}")
+                print("No sampling done...")
+
+            client.wait_for_workers(n_workers)
