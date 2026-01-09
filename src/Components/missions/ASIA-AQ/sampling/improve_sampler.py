cc9d462edbd5a68c110b97327e586c9d601bba63
Patricia Castellanos <patricia.castellanos@nasa.gov>
2026-01-09 15:02:05 -0500

    filter out none ctls instead of testing in loop. use auto chunking, and memory_limit argument

--- src/Components/missions/ASIA-AQ/sampling/improve_sampler.py
+++ src/Components/missions/ASIA-AQ/sampling/improve_sampler.py
@@ -14,13 +14,16 @@ from pyobs.improve import SITE_MAP
 from pyobs.xrctl import parse_ctl
 from dask.distributed import performance_report
 from dask.distributed import Client, LocalCluster
-import dask.dataframe as dd
 if __name__ == '__main__':
 
 
     parser = argparse.ArgumentParser()
     parser.add_argument("config",help='configuration yaml file')
     parser.add_argument("--profile",action="store_true")
+    parser.add_argument("--n_workers",type=int,default=120,
+                        help='number of pool workers to use (default=120)')
+    parser.add_argument("--memory_limist",default='4BG',
+                        help='memory limit per worker (default=4GB)')
 
     args = parser.parse_args()
 
@@ -36,39 +39,37 @@ if __name__ == '__main__':
         os.makedirs(outdir)
 
     # get reference parquet location
-    refdir = config['combined_references']
+    refdir = config['combined_references_dir']
 
-    n_workers = 120
-    kwargs = dict(n_workers=n_workers, threads_per_worker=1, memory_limit='4GB')
+
+    kwargs = dict(n_workers=args.n_workers, threads_per_worker=1, memory_limit=args.memory_limit)
     with LocalCluster(**kwargs) as cluster, Client(cluster) as client:
         print(f"+++++++ IMPROVE Sampling on {site_path}")
-        station,lon,lat = sites.df['SiteCode'].values,sites.df['Longitude'].values,sites.df['Latitude'].values
+        station,lat,lon = sites.df['SiteCode'].values, sites.df['Longitude'].values, sites.df['Latitude'].values
 
         # Sample Aerosol Collection
         # --------------------------------------
         ctls = [config['model_aer_ctl']] + config['model_other_ctl']
-        chunks = {'time':1, 'lev':-1, 'lat':-1, 'lon': -1}
+        ctls = [x for x in ctls if x is not None]
+#        chunks = {'time':1, 'lev':-1, 'lat':-1, 'lon': -1}
+        chunks = 'auto'
         for ctl in ctls:
-            if ctl:
-                # get reference parquet
-                outparq = refdir + f'/reference_store_{ctl}.parq'
-                     
-                print(f" Sampling on reference {outparq}")
-                stn = STATION(station,lon,lat,outparq,verbose=True,chunks=chunks,engine='h5netcdf')
-                    
-                stn_ds = stn.sample()
+            # get reference parquet
+            outparq = refdir + f'/reference_store_{ctl}.parq'
+                 
+            print(f" Sampling on reference {outparq}")                
+            stn = STATION(station,lon,lat,outparq,verbose=True,chunks=chunks,engine='h5netcdf')
+                
+            stn_ds = stn.sample()
 
-                # write out the native sampled model fields
-                outFile = f'{outdir}/improve.{ctl}.nc4'
-                if args.profile:
-                    with performance_report(filename="dask-report.html"):
-                        stn_ds.to_netcdf(outFile,engine='h5netcdf',compute=True)
-                else:
-                    stn_ds.to_netcdf(outFile,engine='h5netcdf',compute=True)
-#                    stn_ds = stn_ds.compute()
-
+            # write out the native sampled model fields
+            outFile = f'{outdir}/improve.{ctl}.nc4'
+            if args.profile:
+                with performance_report(filename="dask-report.html"):
+                    stn_ds = stn_ds.compute()
             else:
-                print(f"Model input files missing for {ctl}")
-                print("No sampling done...")
+                stn_ds = stn_ds.compute()
+            stn_ds.to_netcdf(outFile,format='NETCDF4_CLASSIC')
+            print(f"Successfully wrote {outFile} for {ctl} control file")
 
-            client.wait_for_workers(n_workers)
+            client.wait_for_workers(args.n_workers)
