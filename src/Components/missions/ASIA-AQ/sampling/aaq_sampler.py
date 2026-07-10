7a9c3f87a10975b477c0a4c893bb5cb5d1b7b918
Patricia Castellanos <patricia.castellanos@nasa.gov>
2026-07-10 16:01:48 -0400

    update aaq_sampler to work for cubed sphere

--- src/Components/missions/ASIA-AQ/sampling/aaq_sampler.py
+++ src/Components/missions/ASIA-AQ/sampling/aaq_sampler.py
@@ -17,6 +17,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("config",help='configuration yaml file')
     parser.add_argument("--profile",action="store_true")
+    parser.add_argument("--n_workers",type=int,default=120,
+                        help='number of pool workers to use (default=120)')
+    parser.add_argument("--memory_limit",default='4GB',
+                        help='memory limit per worker (default=4GB)')
 
     args = parser.parse_args()
 
@@ -29,8 +33,9 @@ if __name__ == '__main__':
     # get dc-8 files
     ictFiles = sorted(glob(config['dc8_merge']+'/*ict'))
 
-    kwargs = dict(n_workers=120, threads_per_worker=1, memory_limit='4GB')
+    kwargs = dict(n_workers=args.n_workers, threads_per_worker=1, memory_limit=args.memory_limit)
     with LocalCluster(**kwargs) as cluster, Client(cluster) as cluster:
+        client.wait_for_workers(args.n_workers)
         for ict in ictFiles:
             print(f"+++++++ AAQ Sampling on {ict}")
             m = ICARTT(ict)
@@ -39,6 +44,8 @@ if __name__ == '__main__':
             # Sample Aerosol Collection
             # --------------------------------------
             ctl = config['model_aer_ctl'] 
+            cs  = config.get("cs",False)
+
             chunks = {'time':1, 'lev':-1, 'lat':-1, 'lon': -1}
             # check that all files exist
             time_range = [tyme.min(),tyme.max()]
@@ -51,15 +58,32 @@ if __name__ == '__main__':
                 traj = TRAJECTORY(tyme,lon,lat,ctl,verbose=True,chunks=chunks,engine='h5netcdf')
                 
                 traj_ds = traj.sample()
+
+                outFile = config['sampled_outdir'] + '/' + os.path.basename(ict)[:-3] + ctl + '.nc4'
+                # Check if the file already exists, and clobber (delete) it if it does
+                if os.path.exists(outFile):
+                    os.remove(outFile)
+
                 if args.profile:
                     with performance_report(filename="dask-report.html"):
-                        traj_ds = traj_ds.compute()
-                else:
-                    traj_ds = traj_ds.compute()
+                        if cs:
+                            for vn in traj_ds.data_vars:
+                                print(f'Computing and writing {vn}...')
+                                traj_ds[vn].compute().to_netcdf(outFile, mode='a')  # append mode
+                        else:
+                            traj_ds = traj_ds.compute()
+                            # write out the native sampled model fields
+                            traj_ds.to_netcdf(outFile,engine='netcdf4')
 
-                # write out the native sampled model fields
-                outFile = config['sampled_outdir'] + '/' + os.path.basename(ict)[:-3] + ctl + '.nc4'
-                traj_ds.to_netcdf(outFile,engine='netcdf4')
+                else:
+                    if cs:
+                        for vn in traj_ds.data_vars:
+                            print(f'Computing and writing {vn}...')
+                            traj_ds[vn].compute().to_netcdf(outFile, mode='a')  # append mode
+                    else:
+                        traj_ds = traj_ds.compute()
+                        # write out the native sampled model fields
+                        traj_ds.to_netcdf(outFile,engine='netcdf4')
 
                 # If other model collections are provided
                 # sample those and write out
@@ -86,13 +110,24 @@ if __name__ == '__main__':
                             continue
 
                         traj_ds = traj.sample()
-                        traj_ds = traj_ds.compute()
 
-                        # write out the native sampled model fields
                         outFile = config['sampled_outdir'] + '/' + os.path.basename(ict)[:-3] + ctl + '.nc4'
-                        traj_ds.to_netcdf(outFile,engine='netcdf4')
+                        # Check if the file already exists, and clobber (delete) it if it does
+                        if os.path.exists(outFile):
+                            os.remove(outFile)
+
+                        if cs:
+                            for vn in traj_ds.data_vars:
+                                print(f'Computing and writing {vn}...')
+                                traj_ds[vn].compute().to_netcdf(outFile, mode='a')  # append mode
+                        else:
+                            traj_ds = traj_ds.compute()
+                            # write out the native sampled model fields
+                            traj_ds.to_netcdf(outFile,engine='netcdf4')
 
             else:
                 print(f"Model input files missing for {ict}")
                 print("Skipping...")
                 continue
+
+        client.close()
