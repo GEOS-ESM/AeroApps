33a9a6cebf9720eabf6f14fb1b149babeab25a93
Peter Colarco <peter.r.colarco@nasa.gov>
2026-09-08 15:00:13 -0400

    prc: update fonts

--- src/Components/missions/CPEX-AW/sample/plot_flighttrack.py
+++ src/Components/missions/CPEX-AW/sample/plot_flighttrack.py
@@ -27,7 +27,7 @@ def make_plot():
     ax  = fig.add_subplot(projection=projLcc)
     ax.set_extent([-85,-40,10.,40],crs=ccrs.PlateCarree())
     ax.coastlines(resolution="50m",zorder=99)
-    ax.gridlines(draw_labels=True, dms=True, x_inline=False, y_inline=False, linewidth=1, color='grey')
+    cg = ax.gridlines(draw_labels=True, dms=True, x_inline=False, y_inline=False, linewidth=1, color='grey')
     ax.add_feature(cfeature.BORDERS, edgecolor='black',linewidth=2,zorder=99)
     ax.add_feature(cfeature.STATES, linestyle='--', edgecolor='black', linewidth=1,zorder=99)
     provinc_bodr = cfeature.NaturalEarthFeature(category='cultural',
@@ -35,6 +35,8 @@ def make_plot():
                                                 facecolor='none', edgecolor='k',zorder=99)
     ax.add_feature(provinc_bodr, linestyle='--', linewidth=1, edgecolor="k", zorder=10)
     ax.background_img(name='NE', resolution='higher')
+    cg.xlabel_style = {"size": 22}
+    cg.ylabel_style = {"size": 22}
 
     return ax
 
@@ -62,7 +64,7 @@ if __name__ == "__main__":
         m = ICARTT(ictFile)
         alt, lon, lat, tyme = m.Nav['Altitude'], m.Nav['Longitude'], m.Nav['Latitude'], m.Nav['Time']
         cd = ax.plot(lon,lat,lw=3,transform=ccrs.PlateCarree(),zorder=99,label=ictFile[-15:-7])
-    ax.legend(loc="lower right")
+    ax.legend(loc="upper center",fontsize=24)
     for item in ([ax.title, ax.xaxis.label, ax.yaxis.label] +
              ax.get_xticklabels() + ax.get_yticklabels()):
         item.set_fontsize(24)
